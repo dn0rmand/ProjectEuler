30b5cc41fd58050f623954e09e88c6676a886d53
Dominique NORMAND <dominique@normand.us>
2018-01-25 20:08:13 -0500

    10 done

---
+++ problem10.js
@@ -0,0 +1,34 @@
+const isNumberPrime = require('is-number-prime');
+
+function* primes()
+{
+    yield 2;
+    yield 3;
+
+    let v = 3;
+    while (true)
+    {
+        v++;
+        if (isNumberPrime(v))
+            yield v;
+    }
+}
+
+function solve(maxValue)
+{
+    let primeIterator = primes();
+
+    let p   = primeIterator.next().value; 
+    let sum = 0;
+
+    while (p < maxValue)
+    {
+        sum += p;      
+        p    = primeIterator.next().value; 
+    }
+
+    console.log("Sum of the primes < " + maxValue + " is " + sum);
+}
+
+solve(10);
+solve(2000000);
