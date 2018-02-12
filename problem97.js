9a37293f85da1000858ce59a6bfc0bf99dd5a69d
Dominique NORMAND <dominique@normand.us>
2018-02-12 14:37:01 -0500

    97 done

--- problem97.js
+++ problem97.js
@@ -1,11 +1,11 @@
 const bigInt = require('big-integer');
 
-let max   = bigInt(10000000000);
-let power = bigInt(2);
+let max   = 10000000000;
+let power = 1;
 for(let i = 0; i < 7830457; i++)
 {
-    power = power.multiply(2).mod(max);
+    power = (power * 2) % max;
 }
-let prime = power.multiply(28433).add(1).mod(max);
+let prime = ((power * 28433)+1) % max;
 let p = prime.toString();
 console.log('Result is ' + p + " ( " + p.length + " )");
