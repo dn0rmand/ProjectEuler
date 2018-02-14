55512a20632123425dcbb29cb70822e0023c256b
Dominique NORMAND <dominique@normand.us>
2018-02-13 20:45:57 -0500

    62 done

--- problem62.js
+++ problem62.js
@@ -1,59 +1,26 @@
-const cbrt = require('cbrt');
+let cache = new Map();
 
-function *permutations(startingValue)
+for(let value = 5; ; value++)
 {
-    let digits = [];
-    let v = startingValue;
-    while (v > 0)
+    let cube = value*value*value;
+
+    let key   = cube.toString().split('').sort().join('');
+    let entry = cache.get(key);
+    if (entry === undefined)    
     {
-        digits.push(v % 10);
-        v = (v - (v % 10)) / 10;
+        entry = {
+            value:cube,
+            count:1
+        };
+        cache.set(key, entry);
     }
-
-    function *permute(used, value, count)
+    else
     {
-        if (count === digits.length)
+        entry.count += 1;
+        if (entry.count === 5)
         {
-            if (value !== startingValue)
-                yield value;
-        }
-
-        for(let i = 0; i < digits.length; i++)
-        {
-            let digit = digits[i];
-            if (count === 0 && digit === 0)
-                continue; // Avoid starting with 0
-
-            if (used[i] !== 1)
-            {
-                used[i] = 1;
-                yield *permute(used, value*10 + digit, count+1);
-                used[i] = 0;
-            }
-        }
-    }
-
-    yield *permute([], 0, 0);
-}
-
-for (let i = 5; ; i++)
-{
-    let cube = Math.pow(i, 3);
-
-    let cubes = [cube];
-
-    for(let value of permutations(cube))
-    {
-        let x = Math.pow(value, 1/3);
-        let x2= Math.pow(Math.round(x), 3);
-
-        if (x2 === value)
-        {
-            if (! cubes.includes(value))
-                cubes.push(value);
-        }
-    }
-    if (cubes.length > 3)
-        if (cubes.length === 5)
+            console.log("Smallest cube for which exactly five permutations of its digits are cube is " + entry.value);
             break;
+        }
+    }
 }
