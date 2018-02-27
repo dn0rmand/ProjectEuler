bea859f1dc06cd2cb3b94749029b19d16148c13f
Dominique NORMAND <dominique@normand.us>
2018-02-26 20:19:51 -0500

    94 done ... Not all that fast ( ~ 1 or 2 minutes ) but good enough

--- problem94.js
+++ problem94.js
@@ -1,5 +1,15 @@
+// Almost equilateral triangles
+// Problem 94 
+// It is easily proved that no equilateral triangle exists with integral length sides and integral area. 
+// However, the almost equilateral triangle 5-5-6 has an area of 12 square units.
+
+// We shall define an almost equilateral triangle to be a triangle for which two sides are equal and the third differs 
+// by no more than one unit.
+
+// Find the sum of the perimeters of all almost equilateral triangles with integral side lengths and area and whose 
+// perimeters do not exceed one billion (1,000,000,000).
+
 const assert = require('assert');
-//const bigNum = require('bignumber.js');
 const bigInt = require('big-integer');
 
 const BILLION=1000000000;
@@ -8,8 +18,6 @@ const MAX_SQUAREROOT= bigInt("48112522432468814");
 
 function isPerfectSquare(n)
 {
-    if (n.isZero())
-        return false;
     if (n.equals(1))
         return true;
        
@@ -43,6 +51,8 @@ function isPerfectSquare(n)
 
 function almostEquilateralTriangle(a, b, c)
 {
+    if ((a+b) <= c)
+        return;
 /*
     a + b > c
 
@@ -51,39 +61,59 @@ function almostEquilateralTriangle(a, b, c)
     S needs to be a square multiple of 16
 */
 
-    let s  = (a+b+c) / 2;
-    let sa = s - a;
-    let sb = s - b;
-    let sc = s - c;
+    let s  = a+b+c;
+    let sa = s - a - a;
+    let sb = s - b - b;
+    let sc = s - c - c;
+
     let S  = bigInt(s).multiply(sa).multiply(sb).multiply(sc);
 
-    return isPerfectSquare(S);
+    if (S.isZero())
+        return false;
+
+    if (S.and(0x0F).isZero())
+        return isPerfectSquare(S);
+    else 
+        return false;
 }
 
-//assert.ok(almostEquilateralTriangle(5, 5, 6));
-
-//console.log(682737235+4+5+7);
+assert.ok(almostEquilateralTriangle(5, 5, 6));
 
 let total = bigInt(0);
+let previous = 0;
 
 for(let i = 1; i <= MAX_SIZE; i++)
 {
+    let found = false;
     if (almostEquilateralTriangle(i, i, i+1))
     {
+        found = true;
         let perimeter = i + i + (i+1);
         if (perimeter <= BILLION)
         {
             total = total.add(perimeter);
         }
     }
-    if (almostEquilateralTriangle(i, i+1, i+1))
+    else if (almostEquilateralTriangle(i, i+1, i+1))
     {
+        found = true;
         let perimeter = i + (i+1) + (i+1);
         if (perimeter <= BILLION)
         {
             total = total.add(perimeter);
         }
     }
+    if (found)
+    {
+        if (i > 250)
+        {
+            let j = i-previous;
+            previous = i;
+            i = j * 5;
+        }
+        else
+            previous = i;
+    }
 }
 
-console.log(total.toString() + ' NOT 682737235, 682737251, 156265903821851400, 156265903712058780');
+console.log("Sum of the perimeters of all almost equilateral triangles whose perimeters do not exceed one billion is "+total.toString());
