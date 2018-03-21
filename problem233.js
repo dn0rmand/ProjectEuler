0ff1e723595ea229ee6fda109fab3429caef1e68
Dominique NORMAND <dominique@normand.us>
2018-03-21 15:16:12 -0400

    Work in progress

--- problem233.js
+++ problem233.js
@@ -1,97 +1,39 @@
-/*
+const primes = require('./tools/primes.js');
+const bigInt = require('big-integer');
+const assert = require('assert');
 
-(0,0), (N,0),(0,N), and (N,N)
+const MAX   = 38000000; // 100000000000;
+const MAX_B = 420/4;
 
+const cachedPrimes  = [];
+const memoize       = new Map();
 
-(x – h)2 + (y – k)2 = r2
-
-(0 - h)2 + (0 - k)2 = r2
-(N - h)2 + (0 - k)2 = r2
-(0 - h)2 + (N - k)2 = r2
-(N - h)2 + (N - k)2 = r2
-
-
-h2 = (N-h)2 -> N = 2h -> h = N/2
-k2 = (N-k)2 -> N = 2k -> k = N/2
-
-2*center = N;
-N^2/2 = r2
-2*center*center = r2
-
-(x – h)2 + (y – k)2 = r2
-
-(y – center)^2 + (x – center)^2 = 2*center^2
-y^2 - 2*center*y + center^2 + x^2 - 2*center*x + center^2 = 2*center^2
-y^2 - 2*center*y + x*(x - 2*center) = 0
-
-A = 1
-B = -2*center
-C = x*(x-2*center)
-
-delta^2 = B^2-4AC
-
-y = -center + Math.sqrt((center-x)*(center+x))
-
----------------------------------------------
-
-X = x-center
-Y = y-center
-
-X^2+Y^2=R^2
-
-k = N/2
-
-(X-1/2)^2 + Y^2 = 1/4 * 5^(k-1)
-
-
------------------------
-
-N(r) = R2(r^2) R2(n) is the number of representations of n by summing 2 squares
-*/
-
-const bigNum = require('bigNumber.js');
-const sqrt2  = Math.sqrt(2);
-
-function R2(r)
+function slowF(N)
 {
+    const bigNumber = require('bigNumber.js');
+
+    function round(v)
+    {
+        if (v < 0)
+            return Math.ceil(v)
+        else   
+            return Math.floor(v);
+    }
     
-}
-
-function f(n)
-{
-    let r2 = N*N*2;
-    let N  = R2(r2);
-
-    return N*8 - 4;
-}
-
-function round(v)
-{
-    if (v < 0)
-        return Math.ceil(v)
-    else   
-        return Math.floor(v);
-}
-
-function fx(N)
-{
-    let r2 = N*N*2;
     let center = N/2;
-    let radius = N * sqrt2;
+    let NN = bigNumber(N).times(N);
+    let N4 = bigNumber(N).times(4);
 
     function intersect(x)
     {
-        let delta = N*N + 4*N*x - 4*x*x ;
-        
-        delta = Math.sqrt(delta);
-        let result = (Math.floor(delta) === delta);
+        let delta = NN.plus(N4.times(x)).minus(bigNumber(x).times(x).times(4));
+        delta = delta.squareRoot();
+        let result = delta.isInteger();
+
         return result;
     }
 
-    // intersect(0);
-    // intersect(N);
-
-    let count= 0;
+    let count = 0;
 
     for(let x = round(center); x <= N; x++)
     {
@@ -104,22 +46,151 @@ function fx(N)
     return count*8 - 4;
 }
 
-console.log(f(1328125));
-console.log(f(84246500));
-
-let max = -1;
-let sum = 0;
-for (let N = 0; N <= 38000000/*100000000000*/; N ++)
+function fastF(N)
 {
-    let r =  f(N);
-    if (r === 420)
+    function buildPrimeCache()
     {
-        sum += N;
+        console.log("Initializing Primes ...");
+        let max = 37999991;
+
+        for(let p of primes())
+        {
+            if (p === 2)
+                continue;
+
+            if ((p % 4) === 1)
+                memoize.set(p, 2);
+            else
+                memoize.set(p, 0);
+
+            cachedPrimes.push(p);
+
+            if (p >= max)
+                break
+        }
+        console.log('... Done');
     }
-    // if (r > max)
-    // {
-    //     console.log(N + ' -> ' + r);
-    //     max = r;
-    // }
+
+    function *myPrimes()
+    {
+        if (cachedPrimes.length === 0)
+            buildPrimeCache();
+
+        for(let p of cachedPrimes)
+        {        
+            yield p;
+        }
+        throw "Didn't cache enough primes";
+    }
+
+    function calculateR2(c, primesIterator) 
+    {
+        // Find prime factors
+
+        let B = 1;
+
+        for(let p of primesIterator)
+        {
+            if (p > c)
+                break;
+
+            if (c % p === 0) 
+            {
+                // Determine exponent e of p in c:
+                let e = 0;
+                while (c % p === 0) 
+                {
+                    e += 2; // +2 because didn't do the square of N
+                    c /= p; // Remove this prime factor
+                }
+                
+                if (p % 4 === 1) // Is it a prime congruent 4 (mod 1), raised to an odd power?
+                {             
+                    B *= e+1;
+                }            
+                else if (p % 4 === 3) // Is it a prime congruent 4 (mod 3), raised to an even power?
+                {
+                    if ((e & 1) !== 0) 
+                        return 0;
+                }
+                else
+                    return 0;
+
+                var x = memoize.get(c);
+                if (x !== undefined)
+                {
+                    B *= x;
+                    c = 1;
+                    break;
+                }
+            }
+        }
+
+        // If c > 1 at this point then it is another prime factor:
+        
+        if (c !== 1)
+        {
+            if (c % 4 === 1) 
+            {
+                return B*2;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+
+        return B;
+    };
+
+    function r2(c, primesIterator) 
+    {
+        if (c <= 2)
+            return 0;
+
+        let v = memoize.get(c);
+        if (v !== undefined)
+            return v;
+
+        let B = calculateR2(c, primesIterator);
+
+        //memoize.set(c, B)
+        return B;
+    };
+
+    function f(n)
+    {
+        // Remove even factors:
+        while ((n & 1) === 0)
+        {
+            n >>= 1;
+        }
+
+        result = r2(n, myPrimes());
+        result *= 4;
+
+        return result;
+    }
+
+    return f(N);
 }
-console.log(sum);
+
+let f = fastF;
+
+assert.equal(f(359125),   420);
+assert.equal(f(84246500), 420);
+assert.equal(f(1328125),  180);
+assert.equal(f(248431625),420);
+
+let sum = 0;
+
+for (let N = MAX; N >= 359125; N--)
+{
+    let v = fastF(N);
+    if (v === 420)
+    {
+        sum = sum.plus(N);
+    }
+}
+
+console.log(sum + " ( Should be 30875234922 )");
