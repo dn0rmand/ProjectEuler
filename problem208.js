4ac9be4435804c7540bb621580594c53a0feccb4
Dominique NORMAND <dominique@normand.us>
2018-04-21 09:32:43 -0400

    208 done

--- problem208.js
+++ problem208.js
@@ -1,141 +1,82 @@
 const assert = require('assert');
-const bigNum = require('bigNumber.js');
 const bigInt = require('big-integer');
 
-//bigNum.set({ ROUNDING_MODE: 1, DECIMAL_PLACES: 10 })
+let $direction = 0;
+let $arcs = [[0,0,0,0,0],[0,0,0,0,0]];
 
-const arc = /*72*/ (Math.PI * 2)/5;
-const circle  = 2*Math.PI;
-const cos_arc = bigNum("0.30901699437494745");//Math.cos(arc);
-const sin_arc = bigNum("0.9510565162951535");//Math.sin(arc);
-
-const ERROR = bigNum("2.220446049250313e-16");
-
-let X = bigNum(0);
-let Y = bigNum(0);
-let direction = 0;
-
-function memorizer()
+function isClosed()
 {
-    const memoize = [];
+    function inner(values)
+    {
+        let ref = values[0];
 
-    return {
-        set: function(id, x, y, d, value)
+        if (values[1] === ref && 
+            values[2] === ref && 
+            values[3] === ref && 
+            values[4] === ref)
         {
-            let entry = memoize[id];
-            if (entry === undefined)
-            {
-                entry = new Map();
-                memoize[id] = entry;
-            }
-            x = x.toString();
-            y = y.toString();
-
-            let xE = entry.get(x);
-            if (xE === undefined)
-            {
-                xE = new Map();
-                entry.set(x, xE);
-            }
-            let yE = xE.get(y);
-            if (yE === undefined)
-            {
-                yE = new Map();
-                xE.set(y, yE);
-            }
-            yE.set(d, value);
-        },
-        get: function(id, x, y, d)
-        {
-            let entry = memoize[id];
-            if (entry !== undefined)
-            {
-                x = x.toString();
-                entry = entry.get(x);
-                if (entry !== undefined)
-                {
-                    y = y.toString();
-                    entry = entry.get(y);
-                    if (entry !== undefined)
-                    {
-                        entry = entry.get(d);
-                    }
-                }
-            }
-            return entry;
+            return true;
         }
-    };
+    
+        return false;            
+    }
+
+    if ($direction !== 0)
+        return false;
+
+    let result = inner($arcs[0]) && inner($arcs[1]);
+
+    if (! result)
+        return false;
+
+    return true;
 }
 
-const moveMemoize = memorizer();
+function reset()
+{
+    $direction = 0;
+    $arcs = [[0,0,0,0,0],[0,0,0,0,0]];
+}
 
 function move(clockWise) // 1 or -1
 {
-    let xx = X;
-    let yy = Y;
-    let dd = direction;
+    assert.equal(clockWise === -1 || clockWise === 1, true);
 
-    let result = moveMemoize.get(clockWise+1, xx, yy, dd);
-    if (result !== undefined)
+    if (clockWise === -1)
+        clockWise = 0;
+
+    let index = $direction; // / 72;
+
+    let arcs = $arcs[clockWise];
+
+    let value = arcs[index];
+    value = (value + 1);
+    arcs[index] = value;
+
+    // Some kind of modulo
+    var min = Math.min(arcs[0], arcs[1], arcs[2], arcs[3], arcs[4]);
+    while (min > 0)
     {
-        X = result.X;
-        Y = result.Y;
-        direction = result.direction;
-        return;
+        arcs[0]--;
+        arcs[1]--;
+        arcs[2]--;
+        arcs[3]--;
+        arcs[4]--;
+
+        min--;
     }
 
-    let offsetY = sin_arc;
-    let offsetX = bigNum(1).minus(cos_arc).times(clockWise);
-
-    // Rotate offsets
-    if (direction !== 0)
-    {
-        let cos_dir = Math.cos(direction).toFixed(15);
-        let sin_dir = Math.sin(direction).toFixed(15);
-
-        // x′=xcosθ−ysinθ
-        // y′=ycosθ+xsinθ
-
-        x2 = offsetX.times(cos_dir).minus(offsetY.times(sin_dir));
-        y2 = offsetY.times(cos_dir).plus(offsetX.times(sin_dir));
-
-        offsetX = x2;
-        offsetY = y2;
-    }
-
-    // Add offset
-
-    X = X.plus(offsetX);
-    Y = Y.plus(offsetY);
-
-    // Add rotation
-
-    direction += (arc * clockWise);
-
-    // fix calculation errors
-    while (direction >= circle)
-        direction -= circle;
-    while (direction <= -circle)
-        direction += circle;
-
-    if (X.abs().lte(ERROR))
-        X = bigNum(0);
-
-    if (Y.abs().lte(ERROR))
-        Y = bigNum(0);
-
-    let data = {
-        X: X,
-        Y: Y,
-        direction: direction
-    };
-
-    moveMemoize.set(clockWise+1,  xx, yy, dd, data);
+    if (clockWise)
+        $direction = ($direction+1) % 5;
+    else if ($direction > 0)
+        $direction--;
+    else
+        $direction = 4;
 }
 
 function test()
 {
-    reset(bigNum(0), bigNum(0), 0);
+    reset();
 
     let moves = [
         -1, 1, -3, 4, -1, 2, -1, 1, -4, 1, -2, 1, -3 
@@ -155,98 +96,104 @@ function test()
         }
     }
 
-    return X == 0 && Y == 0;
+    return isClosed();
 }
 
-function reset(x, y, d)
-{
-    X = x;
-    Y = y;
-    direction = d;    
-}
-
-let memoize = memorizer() ;
-    
 function process(totalMoves)
 {
-    let count = bigInt(0);
+    let memoize = {};
+
+    function makeKey(moves)
+    {
+        var key = moves + '-' + $direction + '-' + $arcs.toString();
+
+        return key;
+    }
+
+    function saveState()
+    {
+        // Save State
+        let state = [[],[]];
+
+        for (let i = 0; i < 5; i++)
+        {
+            state[0][i] = $arcs[0][i];
+            state[1][i] = $arcs[1][i];
+        }
+
+        return {
+            direction: $direction,
+            arcs: state
+        };
+    }
+
+    function restoreState(state)
+    {
+        $direction = state.direction;
+
+        for (let i = 0; i < 5; i++)
+        {
+            $arcs[0][i] = state.arcs[0][i];
+            $arcs[1][i] = state.arcs[1][i];
+        }
+    }
 
     function inner(moves)
     {
         if (moves === 0)
         {
-            if (X == 0 && Y == 0)
-                count = count.next();
-            return;
+            if (isClosed())
+                return 1;
+            else
+                return 0;
         }
 
-        let saved = memoize.get(moves, X, Y, direction);
-        if (saved !== undefined)
-        {
-            count = count.plus(saved);
-            return;
-        }
+        let key = makeKey(moves);
 
-        // Save State
+        let cache = memoize[key];
+        if (cache !== undefined)
+            return cache;            
 
-        let oldCount = count;
-        let x = X;
-        let y = Y;
-        let d = direction;
+        let state = saveState();
 
         move(-1);
-        inner(moves-1);
-        reset(x, y, d);
+        let c1 = inner(moves-1);
+
+        restoreState(state);
 
         move(1);
-        inner(moves-1)
-        reset(x, y, d);
+        let c2 = inner(moves-1);
 
-        let added = count.minus(oldCount);
-        memoize.set(moves, X, Y, direction, added);
+        restoreState(state);
+
+        let c ;
+
+        if (bigInt.isInstance(c1))
+            c = c1.plus(c2);
+        else if (bigInt.isInstance(c2))
+            c = c2.plus(c1);
+        else
+        {
+            c = c1+c2;
+            if (c > Number.MAX_SAFE_INTEGER)
+                c = bigInt(c1).plus(c2);
+        }
+
+        memoize[key] = c;
+
+        return c;
     }
 
-    reset(bigNum(0), bigNum(0), 0);
-    inner(totalMoves);
+    reset();
+    let count = inner(totalMoves);
 
     return count;
 }
 
-function passes()
-{
-   let total = bigInt(0);
-
-   let memoize=[];
-
-   function inner(moves)
-   {
-        if (moves === 0)
-        {
-            total = total.next();
-            return;
-        }
-        let t = memoize[moves];
-        if (t !== undefined)
-        {
-            total = total.plus(t);
-            return;
-        }
-        let old = total;
-        inner(moves-1);
-        inner(moves-1);
-        memoize[moves] = total.minus(old);
-   } 
-
-   inner(25);
-   console.log(total.toString());
-}
-
-//passes();
-
 assert.equal(test(), true);
-assert.equal(process(25).valueOf(), 70932);
+assert.equal(process(25), 70932);
 
 let count = process(70);
-console.log(count.toString());
+console.log(count.toString() + " (331951449665644800)");
 
 console.log("Done");
