1ccdacdf0b7b710f76b942d5d029d9ef0bd864e7
Dominique NORMAND <dominique@normand.us>
2018-04-29 13:59:30 -0400

    fixed

--- tools/totient.js
+++ tools/totient.js
@@ -2,7 +2,6 @@ const f = function()
 {
     const $isPrime = require("./isPrime.js");
     const $primes  = require('./primes.js');
-    const sqlite   = require('better-sqlite3');
 
     let _maximum     = 4;
     let _maxPrime    = 3;
@@ -16,7 +15,20 @@ const f = function()
         if ((p & 1)  === 0 || (p % 3) === 0)
             return false;
         let root = Math.floor(Math.sqrt(p));
-        for (let i = 5; i <= root; i += 6) 
+
+        let lastPrime = 5;
+
+        for(let i of _primeTable.keys())
+        {
+            if (i > root)
+                return true;
+            if (p % i === 0)
+                return false;
+
+            lastPrime = i;
+        }
+
+        for (let i = lastPrime; i <= root; i += 6) 
         {
             if (p % i === 0 || p % (i + 2) == 0)
                 return false;
@@ -60,32 +72,6 @@ const f = function()
     }
 
     let totient = {
-        load: function()
-        {
-            _primeTable.clear();
-            let db = new sqlite('../data/primes.sqlite3', { fileMustExist: true });
-            let command = db.prepare('SELECT prime FROM primes');
-            for (let row of command.iterate()) 
-            {
-                let p = +(row.prime);
-                _primeTable.set(p);
-                _maxPrime = p;
-                _maximum = p;
-            }            
-        },
-        save: function()
-        {
-            let db = new sqlite('../data/primes.sqlite3', { fileMustExist: false });
-
-            db.exec("DROP TABLE primes");
-            db.exec("CREATE TABLE primes(prime INTEGER)");
-            db.exec("BEGIN");
-            let command = db.prepare("INSERT INTO primes (prime) VALUES (?)");
-            for (let p of this.primes())
-                command.run(p);
-            db.exec("COMMIT");
-            db.close();                    
-        },
         // You can overide this to define you own prime implementation
         initialize: function(max) 
         {
