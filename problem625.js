80b49a5f647d88f9979f80f7c66a4b2fc4740603
Dominique NORMAND <dominique@normand.us>
2018-04-29 13:59:39 -0400

    backup

--- problem625.js
+++ problem625.js
@@ -9,15 +9,57 @@
 const assert    = require('assert');
 const prettyTime= require("pretty-hrtime");
 const cluster   = require('cluster');
+const bitBuffer = require('bitbuffer');
 
 const MODULO = 998244353;
 
-const MAX  = Math.pow(10, 11);
-const TEST = Math.pow(10, 8);
+const MAX           = Math.pow(10, 11);
+const TEST          = Math.pow(10, 8);
+const ARRAY_SIZE    = MAX/8;
 
 const _primeMap    = new Map();
 let   _primes      = []
 let   _maxPrime    = 3;
+
+console.log("Initializating arrays");
+let caches = [
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//1
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//2
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//3
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//4
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//5
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//6
+    new bitBuffer.BitBuffer(ARRAY_SIZE),//7
+    new bitBuffer.BitBuffer(ARRAY_SIZE) //8
+];
+console.log("Arrays now ready");
+
+caches.total = 0;
+
+caches.get = function(index)
+{    
+    index--;
+
+    let offset  = index % ARRAY_SIZE;
+    let array   = (index - offset) / ARRAY_SIZE;
+
+    array = caches[array];
+
+    return array.get(offset);
+}
+
+caches.set = function(index)
+{
+    index--;
+
+    let offset  = index % ARRAY_SIZE;
+    let array   = (index - offset) / ARRAY_SIZE;
+    
+    array = caches[array];
+
+    array.set(offset, 1);
+    caches.total++;
+}
 
 function isPrime(p)
 {
@@ -127,6 +169,112 @@ function g(n, e)
     }
 }
 
+function seed(max)
+{    
+    let timer = process.hrtime();
+    
+    let total = 1; // 1 => 1
+    caches.set(1);
+
+    function add(index, value)
+    {
+        // if (! caches.get(index))
+        // {
+        total = (total + value) % MODULO;
+        caches.set(index);
+        // }  
+    }
+
+    function inner(pi, factor, value, index)
+    {
+        if (index > max)
+            return;
+        
+        let prime = _primes[pi];
+
+        let newValue = value * g(prime, factor);
+        add(index, newValue);
+
+        // Use same prime ( special case )
+        let idx = index*prime;
+        let f = factor;
+        if (idx > max)
+            return;
+            
+        while (idx <= max)
+        {
+            f++;
+
+            let v = value * g(prime, f);
+            add(idx, v);
+
+            for(let pj = pi+1; pj < _primes.length; pj++)
+            {
+                let p = _primes[pj];
+                let i = idx*p;
+                if (i > max)
+                    break;
+                inner(pj, 1, v, i);
+            }
+
+            idx *= prime;
+        }
+
+        // use other primes
+        for(let pj = pi+1; pj < _primes.length; pj++)
+        {
+            // only primes not already used
+            let p = _primes[pj];
+            idx = index*p;
+            if (idx > max)
+                break;
+            inner(pj, 1, newValue, idx);
+        }
+    }
+
+    console.log("Seeding ...");
+    for(let pi = 0; pi < _primes.length; pi++)
+    {
+        inner(pi, 1, 1, _primes[pi]);
+    }
+
+    console.log("Seeded");
+    console.log((max - caches.total) + " values remaining");
+
+    for(let i = 2; i <= max; i++)
+    {        
+        if (! caches.get(i))
+        {
+            // Some verification
+            if (! isPrime(i))
+                throw i + " is not a prime!!!";
+
+            console.log("Found new prime " + i + "... seeding some more ...");
+            let v = g(i, 1);
+
+            add(i, v);
+
+            for(let pi = 0; pi < _primes.length; pi++)
+            {
+                let idx = i * _primes[pi];
+                if (idx > max)
+                    break;
+
+                inner(pi, 1, v, idx);
+            }
+
+            console.log("Seeded");
+            console.log((max - caches.total) + " values remaining");
+            // total = (total + g(i, 1)) % MODULO;
+        }
+    }
+
+    timer = process.hrtime(timer);    
+    console.log('seed(' + max + ') = ' + total + ' - Calculated in ' + prettyTime(timer, {verbose:true}));
+    
+    return total;
+}
+
 function G(min, max)
 {
     let timer = process.hrtime();
@@ -157,17 +305,30 @@ function solve(start, end, runTests)
 
     console.log("initializing primes");
     let timer = process.hrtime();
-    generatePrimes(10000000);
+    generatePrimes(100000000);                        
     timer = process.hrtime(timer);
     console.log('primes loaded in ' + prettyTime(timer, {verbose:true}));
+
+    let answer = seed(9999999999);
+//    let answer = seed(MAX);
+    console.log("ANSWER is " + answer + " - expected = 551614306");
+    process.exit(0);
 
     // Tests
 
     if (runTests === true)
     {
-        assert.equal(G(1, 10), 122);
-        assert.equal(G(1, 1000), 2475190);
-        assert.equal(G(1, 10000), 317257140);
+        assert.equal(seed(50000000), 954109446); 
+
+        // assert.equal(G(1, 10), 122);
+        // assert.equal(G(1, 1000), 2475190);
+        // assert.equal(G(1, 10000), 317257140);
+        // assert.equal(G(1, 10000000), 825808541);     
+        // assert.equal(G(1, 20000000), 974543073);  
+        
+        assert.equal(G(1, 50000000), 954109446);       
+        
+        // G(50000000) = 954109446 - Calculated in 3 minutes 5 seconds 520 milliseconds 159 microseconds 936 nanoseconds        
     }
     else
     {
@@ -189,9 +350,11 @@ function solve(start, end, runTests)
 // 1 to 9999999999 -> 338711832
 // ------------------------------------------------
 
+
 if (cluster.isMaster) 
 {
     solve(1, 100000, true);
+    process.exit(0);
 
     console.log("I'm the master");
 
@@ -247,3 +410,5 @@ else
         process.exit(0);      
     });
 }
+
+// 551614306
