32aad1e9c140d309916649dae3aab71ce683c503
Dominique NORMAND <dominique@normand.us>
2018-05-05 22:43:52 -0400

    293 done .. No very fast as usual

---
+++ problem293.js
@@ -0,0 +1,84 @@
+// Pseudo-Fortunate Numbers
+// ------------------------
+// Problem 293 
+// -----------
+// An even positive integer N will be called admissible, if it is a power of 2 or its distinct 
+// prime factors are consecutive primes.
+// The first twelve admissible numbers are 2,4,6,8,12,16,18,24,30,32,36,48.
+
+// If N is admissible, the smallest integer M > 1 such that N+M is prime, will be called the 
+// pseudo-Fortunate number for N.
+
+// For example, N=630 is admissible since it is even and its distinct prime factors are the 
+// consecutive primes 2,3,5 and 7.
+// The next prime number after 631 is 641; hence, the pseudo-Fortunate number for 630 is M=11.
+// It can also be seen that the pseudo-Fortunate number for 16 is 3.
+
+// Find the sum of all distinct pseudo-Fortunate numbers for admissible numbers N less than 1000000000.
+
+const assert = require('assert');
+const primeHelper = require('./tools/primeHelper.js')();
+
+const MAX = 1000000000;
+
+primeHelper.initialize(Math.floor(Math.sqrt(MAX)));
+
+function admissible(n)
+{
+    if ((n & 1) !== 0)
+        return false;
+
+    if (n === 2)
+        return true;
+
+    for (let p of primeHelper.primes())
+    {
+        if (p > n)
+            break;
+        
+        if (n % p !== 0)
+            return false;
+        
+        while ((n % p) === 0)
+            n /= p;
+
+        if (n === 1)
+            break;
+    }
+
+    return n === 1;
+}
+
+function pseudoFortunate(n)
+{
+    let m = n+3;
+    while(! primeHelper.isPrime(m))
+        m += 2;
+    return m-n;
+}
+
+function *getAdmissible(max)
+{
+    for(let n = 2; n < max; n += 2)
+    {
+        if (admissible(n))
+            yield n;
+    }
+}
+
+assert.equal(pseudoFortunate(630), 11);
+
+let used = new Map();
+let total= 0;
+
+for(let a of getAdmissible(MAX))
+{
+    let m = pseudoFortunate(a);
+    if (! used.has(m))
+    {
+        used.set(m);
+        total += m;
+    }
+}
+
+console.log("The sum of all distinct pseudo-Fortunate numbers for admissible numbers N less than 1000000000 is " + total);
---
+++ tools/primeHelper.js
@@ -0,0 +1,76 @@
+module.exports = function()
+{
+    const _primeMap    = new Map();
+    let   _primes      = []
+    let   _maxPrime    = 0;
+
+    function isPrime(p)
+    {
+        if (_primeMap.has(p))
+            return true;
+        if (p <= _maxPrime)
+            return false;
+
+        if ((p & 1)  === 0 || (p % 3) === 0)
+            return false;
+        let root = Math.floor(Math.sqrt(p));
+        for(let i of _primes)
+        {
+            if (i > root)
+                break;
+            if (p % i === 0)
+                return false;
+        }
+        return true;                        
+    }
+
+    function generatePrimes(max) 
+    {
+        _primeMap.set(2);
+        _primeMap.set(3);
+        _primes.push(2);
+        _primes.push(3);
+
+        let n = max || Math.ceil(Math.sqrt(MAX));
+        
+        let sieve = []; //new Int32Array(max / 31);
+
+        for (let i = 2, j = 3; ; i+=2, j+=3)
+        {
+            if (i > n)
+                break;
+            sieve[i] = 0;
+            if (j <= n)
+                sieve[j] = 0;
+        }
+
+        for (let i = 5; i <= n; i += 2) 
+        {
+            if (sieve[i] === 0)
+                continue;
+            sieve[i] = 1;
+            _primes.push(i);
+            _primeMap.set(i);
+            _maxPrime = i;
+
+            for(let j = i+i; j <= n; j += i)
+            {
+                sieve[j] = 0;
+            }
+        }
+
+        _maxPrime = max;
+    }
+
+    return {
+        initialize: function(max) {
+            generatePrimes(max);
+        },
+        primes : function *() {
+            yield *_primeMap.keys();
+        },
+        isPrime: function(value) { 
+            return isPrime(value); 
+        }
+    }
+}
