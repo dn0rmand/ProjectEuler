34d98b3855ddb594a4778448e9907d1b90899bfe
Dominique NORMAND <dominique@normand.us>
2018-05-13 12:19:04 -0400

    Reworked to no need to modify ml-matrix

--- problem258.js
+++ problem258.js
@@ -8,6 +8,8 @@
 //
 // Find gk mod 20092010 for k = 10^18.
 
+'use strict';
+
 const assert = require('assert');
 const {Matrix} = require('ml-matrix');
 const bigInt = require('big-integer');
@@ -17,101 +19,137 @@ const LAG     = 2000; // 00;
 const MODULO  = 20092010;
 const BIG_MODULO = 20092010 * 1000;
 
+class DNMatrix extends Matrix 
+{
+    static get [Symbol.species]() 
+    {
+      return this;
+    }
+
+    mmul(other) 
+    {
+        other = this.constructor.checkMatrix(other);
+        if (this.columns !== other.rows) 
+        {
+            // eslint-disable-next-line no-console
+            console.warn('Number of columns of left matrix are not equal to number of rows of right matrix.');
+        }
+
+        var m = this.rows;
+        var n = this.columns;
+        var p = other.columns;
+
+        var result = new this.constructor[Symbol.species](m, p);
+
+        var Bcolj = new Array(n);
+        for (var j = 0; j < p; j++) 
+        {
+            for (var k = 0; k < n; k++) 
+            {
+                Bcolj[k] = other.get(k, j);
+            }
+
+            for (var i = 0; i < m; i++) 
+            {
+                var s = 0;
+                for (k = 0; k < n; k++) 
+                {
+                    s = (s + (this.get(i, k) * Bcolj[k])) % 20092010;
+                }
+
+                result.set(i, j, s);
+            }
+        }
+
+        return result;
+    }
+
+    static create()
+    {
+        let matrix = this.zeros(LAG, LAG);
+
+        let i;
+
+        for (let r = 1; r < LAG; r++)
+        {
+            matrix.set(r, r-1, 1);
+        }
+
+        matrix.set(0,LAG-1,1);
+        matrix.set(0,LAG-2,1);
+        return matrix;
+    }
+
+    static createVector()
+    {
+        let vector = this.zeros(LAG, 1);
+
+        for (let r = 0; r < LAG; r++)
+        {
+            vector.set(r, 0, G(r));
+        }
+
+        return vector;
+    }
+
+    pow(pow)
+    {
+        let m  = this;
+        let mm = undefined;
+
+        if (pow === 1)
+            return m;
+
+        while (pow > 1)
+        {
+            if ((pow & 1) !== 0)
+            {
+                console.log(pow);
+                if (mm === undefined)
+                    mm = m;
+                else
+                    mm = mm.mmul(m);
+
+                pow--;
+            }
+
+            while (pow > 1 && (pow & 1) === 0)
+            {
+                console.log(pow);
+                pow /= 2;
+                m =  m.mmul(m);
+            }
+        }
+
+        if (mm !== undefined)
+        {
+            console.log(pow);
+            m = m.mmul(mm);
+        }
+        
+        return m;
+    }
+}
+
+let memoize = new Map();
+
 function G(n)
 {
     if (n < LAG)
         return 1;
-    return G(n-LAG) + G(n-LAG+1); 
-}
-
-function makeMatrix()
-{
-    let matrix = Matrix.zeros(LAG, LAG);
-
-    let i;
-
-    for (let r = 1; r < LAG; r++)
+    let result = memoize.get(n);
+    if (result !== undefined)
     {
-        matrix.set(r, r-1, 1);
+        return result;
     }
-
-    matrix.set(0,LAG-1,1);
-    matrix.set(0,LAG-2,1);
-    return matrix;
-}
-
-function makeVector()
-{
-    let vector = Matrix.zeros(LAG, 1);
-
-    for (let r = 0; r < LAG; r++)
+    else
     {
-        vector.set(r, 0, G(r));
+        result = (G(n-LAG) + G(n-LAG+1)) % MODULO; 
+        memoize.set(n, result);
+        return result;
     }
-
-    return vector;
 }
 
-function multiply(m1, m2)
-{
-    // let rows    = m1.rows;
-    // let columns = m1.columns;
-
-    // let result = new Matrix(rows, columns);
-    // let cache  = new Array(columns);
-
-    // for (let col = 0; col < columns; col++) 
-    // {
-    //     for (let col2 = 0; col2 < columns; col2++) 
-    //         cache[col2] = m2.get(col2, col);
-
-    //     for (let row = 0; row < rows; row++) 
-    //     {
-    //         let s = 0;
-    //         for (let col2 = 0; col2 < columns; col2++)
-    //             s += (m1.get(row, col2) * cache[col2]) % BIG_MODULO;
-
-    //         result.set(row, col, s);
-    //     }
-    // }
-
-    let result = m1.mmul(m2);
-    
-    return result;
-}
-
-function matrixPower(m, pow)
-{
-    let mm = undefined;
-
-    if (pow === 1)
-        return m;
-
-    while (pow > 1)
-    {
-        if ((pow & 1) !== 0)
-        {
-            if (mm === undefined)
-                mm = m;
-            else
-                mm = multiply(mm, m);
-
-            pow--;
-        }
-
-        while (pow > 1 && (pow & 1) === 0)
-        {
-            console.log(pow);
-            pow /= 2;
-            m =  multiply(m, m);
-        }
-    }
-
-    if (mm !== undefined)
-        m = multiply(mm, m);
-
-    return m;
-}
 function *GENERATE()
 {
     let cache = [];
@@ -125,7 +163,7 @@ function *GENERATE()
 
     while (1)
     {
-        let v = (cache[idx] + cache[(idx+1) % LAG]);// % MODULO;
+        let v = (cache[idx] + cache[(idx+1) % LAG]) % MODULO;
 
         yield v;
 
@@ -134,22 +172,21 @@ function *GENERATE()
     }
 }
 
-console.log("Make sure to modify ml-matrix mmul to include the modulo");
+let matrix = DNMatrix.create();
+let vector = DNMatrix.createVector();
 
-let matrix = makeMatrix();
-// printM(matrix);
+let T = Math.pow(10, /*5*/18);
 
-let v = makeVector();
-let T = Math.pow(10, 18);
+matrix = matrix.pow(T);
+vector = matrix.mmul(vector);
 
-rows = matrixPower(matrix, T);
-rows = rows.mmul(v).mod(MODULO);
+let target = vector.get(LAG-1, 0);
 
-let target = rows.get(LAG-1, 0);
-//let x = G(T);
-
-//assert.equal(target, x);
-
-console.log('G(' + T + ') = ' + target);
-console.log("FYI, 3394748 is incorrect.")
-console.log('Done');
+if (T <= 100000)
+{
+    let x = G(T);
+    assert.equal(target, x);
+    console.log('Done');
+}
+else
+    console.log('G(' + T + ') = ' + target + " - Should be 12747994");
