56e12967c6619bd97548606fc4e70f22b71aa3e2
Dominique NORMAND <dominique@normand.us>
2018-05-31 08:50:56 -0400

    Faster but still slow and uses too much memory

--- problem393.js
+++ problem393.js
@@ -13,162 +13,257 @@
 
 const assert = require('assert');
 const bigInt = require('big-integer');
+const prettyHrtime = require("pretty-hrtime");
 
-const UP    = 1;
-const DOWN  = 2;
-const LEFT  = 3;
-const RIGHT = 4;
+const UP    = 1; 
+const DOWN  = 2; 
+const LEFT  = 3; 
+const RIGHT = 4; 
+
+class Memoize 
+{
+    constructor()
+    {
+        this.content = new Map();
+        this.size = 0;
+    }
+
+    get(key1, key2, key3)
+    {
+        let inner = this.content.get(key1.valueOf());
+        if (inner === undefined)
+            return undefined;
+        inner = inner.get(key2.valueOf());
+        if (inner === undefined)
+            return undefined;
+
+        let r = inner.get(key3.valueOf());
+        return r;
+    }
+
+    set(key1, key2, key3, value)
+    {
+        let inner = this.content.get(key1.valueOf());
+        if (inner === undefined)
+        {
+            inner = new Map();
+            this.content.set(key1.valueOf(), inner);
+        }
+        let inner2 = inner.get(key2.valueOf());
+        if (inner2 === undefined)
+        {
+            inner2 = new Map();
+            inner.set(key2.valueOf(), inner2);
+        }
+        inner2.set(key3.valueOf(), value);
+        this.size++;
+    }
+}
 
 class Row
 {
     constructor(item)
     {
-        this.isRow = true;
+        this.up     = 0;
+        this.down   = 0;
 
         if (item === undefined)
-            this.value = [];
+            this.value = 0;
         else
-            this.value = [item];
+        {
+            this.value = item;
+            if (item === UP)
+                this.up++;
+            else if (item === DOWN)
+                this.down++;
+        }
     }
 
     clone()
     {
         let r = new Row();
-        r.value = Array.from(this.value);
+
+        r.value  = this.value;
+        r.down   = this.down;
+        r.up     = this.up;
+
         return r;
     }
 
     isEmpty() 
     { 
-        return this.value.length === 0; 
+        return this.value === 0; 
     }
 
     push(item)
     {
-        this.value.push(item);
+        this.value = this.value*10 + item;
+
+        if (item === DOWN)
+            this.down++;
+        else if (item === UP)
+            this.up++;
     }
 
     pop()
     {  
-        this.value.pop();
+        let d = this.value % 10;
+        this.value = (this.value - d) / 10;
+        if (d === UP)
+            this.up--;
+        else if (d === DOWN)
+            this.down--;
     }
 
-    get(index)
+    get(index, length)
     {
-        if (index < 0 || index >= this.value.length)
-            return ' ';
+        if (index < 0 || index >= length)
+            return 0;
         else   
-            return this.value[index];
+        {
+            index = length - index - 1;
+            let value = this.value; 
+            while (index-- > 0)
+            {
+                value = (value - (value % 10)) / 10;
+            }
+            return value % 10;
+        }
     }
 
     contains(item)
     {
-        return this.value.includes(item);
+        if (item === DOWN)
+            return this.down !== 0;
+        else if (item === UP)
+            return this.up !== 0;
+
+        let v = this.value;
+        while (v > 0)
+        {
+            let d = v % 10;
+            if (d === item)
+                return true;
+            v = (v-d) / 10;
+        }
+        return false;
     }
 
-    getLength() 
+    countOf(item)
     {
-        return this.value.length; 
+        if (item === UP)
+            return this.up;
+        else if (item === DOWN)
+            return this.down;
+
+        return 0;
     }
 
     valueOf()
     {
-        return this.value.join('');
+        return this.value;
     }
 }
 
-function f(size)
+function f(size, trace)
 {
-    let memoize = {};
+    let memoize = new Memoize();
     
     function makeRows(above, aboveAbove)
     {
-        if (above === undefined)
-            above = new Row();
-        else if (above.isRow !== true)
-            above = new Row(above);
+        let result;
 
-        if (aboveAbove === undefined)
-            aboveAbove = new Row();
-        else if (aboveAbove.isRow !== true)
-            aboveAbove = new Row(aboveAbove);
-
-        function *inner(row)
+        function inner(row, x)
         {
             if (row === undefined)
             {
-                if (above.get(1) != DOWN)
+                if (above.isEmpty())
                 {
-                    yield *inner(new Row(RIGHT));
+                    inner(new Row(RIGHT), 1);
                 }
+                else
+                {
+                    if (above.get(1, size) != DOWN)
+                    {
+                        inner(new Row(RIGHT), 1);
+                    }
 
-                yield *inner(new Row(DOWN));
+                    inner(new Row(DOWN), 1);
 
-                if (above.get(0) != DOWN && above.get(1) != LEFT && aboveAbove.get(0) !== DOWN)
-                {                         
-                    yield *inner(new Row(UP));                    
+                    if (above.get(0, size) != DOWN && above.get(1, size) != LEFT && aboveAbove.get(0, size) !== DOWN)
+                    {                         
+                        inner(new Row(UP), 1);                    
+                    }
                 }
                 return;
             }
 
-            let x = row.getLength();
-
             if (x >= size)
             {
-                yield row.clone();          
+                // Ensure correct !!!!
+                if (! above.isEmpty())
+                {
+                    // if (above.countOf(UP) != r.countOf(DOWN))
+                    //     continue;
+                    if (above.countOf(DOWN) != row.countOf(UP))
+                        return; // Invalid row
+                }
+                else // First row so no UP and start with RIGHT
+                {
+                    if (row.get(0, size) !== RIGHT || row.contains(UP))
+                        return;
+                }
+                
+                result.push(row.clone());
                 return;
             }
 
             let lastX = (x === (size-1));
                 
-            if (row.get(x-1) === LEFT && above.get(x-1) != DOWN)
+            if (row.get(x-1, x) === LEFT && above.get(x-1, size) != DOWN)
             {
                 row.push(LEFT);
-                yield *inner(row);
+                inner(row, x+1);
                 row.pop();
             }
-            else if (row.get(x-1) === RIGHT)
+            else if (row.get(x-1, x) === RIGHT)
             {
                 // Cannot do L
             }
-            else if (row.get(x-2) != RIGHT && above.get(x-1) != DOWN)
+            else if (row.get(x-2, x) != RIGHT && above.get(x-1, size) != DOWN)
             {
                 row.push(LEFT);
-                yield *inner(row);
+                inner(row, x+1);
                 row.pop();
             }
     
-            if (! lastX && above.get(x+1) != DOWN) // Cannot go right on the right size
+            if (! lastX && above.get(x+1, size) != DOWN) // Cannot go right on the right size
             {
                 row.push(RIGHT);
-                yield *inner(row);
+                inner(row, x+1);
                 row.pop();
             }
 
             row.push(DOWN);
-            yield *inner(row);
+            inner(row, x+1);
             row.pop();
 
-            if (above.get(x) != DOWN && above.get(x-1) !== RIGHT && 
-                above.get(x+1) !== LEFT && aboveAbove.get(x) !== DOWN)
+            if (above.get(x, size) != DOWN && above.get(x-1, size) !== RIGHT && 
+                above.get(x+1, size) !== LEFT && aboveAbove.get(x, size) !== DOWN)
             {
                 row.push(UP);
-                yield *inner(row);
+                inner(row, x+1);
                 row.pop();
             }
         }
 
-        let key    = aboveAbove.valueOf() + '-' + above.valueOf();
-        let result = memoize[key];
+        result = memoize.get(-1, aboveAbove, above);
 
         if (result === undefined)
         {
             result = [];
-
-            for(let r of inner())
-                result.push(r);
-
-            memoize[key] = result;
+            inner();
+            memoize.set(-1, aboveAbove, above, result);
             return result;
         }
         else
@@ -179,9 +274,7 @@ function f(size)
 
     function execute(above, aboveAbove, rowCount)
     {
-        let key = above.valueOf()+'-'+aboveAbove.valueOf()+'-'+rowCount;
-
-        let total = memoize[key];
+        let total = memoize.get(rowCount, aboveAbove, above);
         if (total !== undefined)
             return total;
 
@@ -190,25 +283,19 @@ function f(size)
         let rows = makeRows(above, aboveAbove);
         let count= rows.length;
 
-        if (rowCount === 1)
-        {
-            while (count > 0 && rows[count-1].get(0) !== RIGHT)
-            {
-                count--;
-            }
-        }
-
         for (let index = 0; index < count; index++)
         {
             let row = rows[index];
 
             if (rowCount === 1)
             {
-                // let percent = ((index / count)*100).toFixed(2);
-                // console.log(percent);
+                if (trace)
+                {
+                    let percent = ((index / count)*100).toFixed(2);
+                    console.log(percent);
+                }
 
-                if (! row.contains(UP)) // cannot go up from first row
-                    total = total.plus( execute(row, above, rowCount+1) );
+                total = total.plus( execute(row, above, rowCount+1) );
             }
             else if (rowCount === size)
             {
@@ -224,23 +311,29 @@ function f(size)
         if (total.isSmall)
             total = total.valueOf();
 
-        memoize[key] = total;
+        memoize.set(rowCount, aboveAbove, above, total);
+
         return total;
     }
 
-    let total = execute(new Row(), new Row(), 1) * 2;
+    let start = process.hrtime();
+    let total = execute(new Row(), new Row(), 1);
+    total = bigInt(2).times(total);
+    let end = process.hrtime(start);
 
-    console.log("f("+size+") = " + total.toString());
+    console.log("f("+size+") = " + total.toString() + ", processed in " + prettyHrtime(end, {verbose:true}));
     return total;
 }
 
 // "112398351350823112"
 
-// console.log(f(2));
-f(4);
-f(6);
-//f(8);
+assert.equal(f(4).valueOf(), 88);
+assert.equal(f(6).valueOf(), 207408);
 
-//assert.equal(f(4), 88);
+console.log('f(8)');
+f(8, true); // 22902801416
+console.log('f(10)');
+f(10, true); // "112398351350823112"
+
 
 console.log('Done');
