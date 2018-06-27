94dcfd37c286eff845443f5dc94c4b09279a91d7
Dominique NORMAND <dominique@normand.us>
2018-06-27 08:59:06 -0500

    229 done

--- problem229.js
+++ problem229.js
@@ -1,53 +1,6 @@
 const MAX_SAMPLE = 10000000;
 const MAX = 2000000000;
 
-const primeHelper = require('./tools/primeHelper')();
-
-primeHelper.initialize(Math.floor(Math.sqrt(MAX)));
-
-function *filter(max)
-{
-    for (let value = 8; value < max; value++)
-    {
-        if (primeHelper.isPrime(value))
-            continue;
-
-        let v = value;
-        let good = true;
-        for (let p of primeHelper.primes())
-        {
-            if (p > v)
-                break;
-
-            if (v % p === 0)
-            {
-                let count = 1;
-                v /= p;
-                while (v % p === 0)
-                {
-                    count++;
-                    v /= p;
-                }
-
-                if (p % 4 === 3 && (count & 1) !== 0)
-                {
-                    good = false;
-                    break;
-                }
-
-                if (primeHelper.isPrime(v))
-                {
-                    if (v % 4 === 3)
-                        good = false;
-                    break;
-                }
-            }
-        }
-        if (good)
-            yield value;
-    }
-}
-
 function solve(max)
 {
     let values = new Int8Array(max+1);
@@ -90,16 +43,6 @@ function solve(max)
     }
 
     return total;
-}
-
-function dummy()
-{
-    let total = 0;
-    for (let value of filter(MAX))
-    {
-        total++;
-    }
-    console.log(total);
 }
 
 let answer = solve(MAX);
