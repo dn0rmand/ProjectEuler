114004c1048c8f1ae47285e2e0e9c61230f458a5
Dominique NORMAND <dominique@normand.us>
2018-07-19 21:12:20 -0400

    Brute force!!!!!

--- problem323.js
+++ problem323.js
@@ -1,64 +1,123 @@
-const BIT16 = Math.pow(2, 16);
-const BIT15 = BIT16-1;
 
-function *Y()
-{
-    while (true)
-    {
-        let r1 = Math.random();
-        let y1 = Math.floor(r1*BIT16) 
-        let r2 = Math.random();
-        let y2 = Math.floor(r2*BIT16) 
-        
-        yield { y1:y1, y2:y2 };
-    }
-}
-
-function N()
-{
-    let i = 0
-    let x1 = 0;
-    let x2 = 0;
-
-    for (let y of Y())
-    {
-        i++;
-        x1 |= y.y1;
-        x2 |= y.y2;
-        if (x1 === BIT15 && x2 === BIT15)
-            return i;
-    }
-}
+const announce = require('./tools/announce');
+const fs = require('fs');
+const Chance = require('chance');
+const chance = new Chance();
 
 function bruteForce()
 {
-    let total  = 0;
-    let old    = 0;
-    let stable = 100;
-    let i      = 0;
+    const BIT16 = Math.pow(2, 16);
+    const BIT15 = BIT16-1;
 
-    while(++i)
+    function *Y()
     {
-        total = total + N();
-        if (total > Number.MAX_SAFE_INTEGER)
-            throw "NEED BIGINT";
-        let v = Math.floor((total / i)*10000000000);
-        if (old === v)
+        while (true)
         {
-            stable--;
-            if (stable === 0)
-                return v / 10000000000;
-        }
-        else
-        {
-            stable = 100;
-            old    = v;
-            if ((i % 1000000) === 0)
-                process.stdout.write('\r' + v + '    ');
+            let y1  = chance.integer({min:0, max:BIT15});
+            let y2  = chance.integer({min:0, max:BIT15});
+            
+            yield { y1:y1, y2:y2 };
         }
     }
+
+    function N()
+    {
+        let i = 0
+        let x1 = 0;
+        let x2 = 0;
+
+        for (let y of Y())
+        {
+            i++;
+            x1 |= y.y1;
+            x2 |= y.y2;
+            if (x1 === BIT15 && x2 === BIT15)
+                return i;
+        }
+    }
+
+    function solve()
+    {
+        let total  = 0;
+        let old    = 0;
+        let stable = 100;
+        let i      = 0;
+
+        let input = fs.readFileSync("323.data");
+        input = JSON.parse(input);
+        total = input.total; // 40170917412;
+        i     = input.count; // 6321000000;
+
+        while(++i)
+        {
+            total = total + N();
+            if (total > Number.MAX_SAFE_INTEGER)
+            {
+                announce(323, "Need Big-Integer :(" );
+                throw "NEED BIGINT";
+            }
+            let v = Math.floor((total / i)*10000000000);
+            if (old === v)
+            {
+                stable--;
+                if (stable === 0)
+                    return v / 10000000000;
+            }
+            else
+            {
+                stable = 100;
+                old    = v;
+                if ((i % 1000000) === 0)
+                {
+                    fs.writeFileSync("323.data", JSON.stringify({ total: total, count: i}));                    
+                    process.stdout.write('\r' + total + '/' + i + ' - ' + v);
+                }
+            }
+        }
+    }
+
+    let v1 = solve();
+    console.log('\n');
+    announce(323, 'Answer is' + v1);
 }
 
-let v1 = bruteForce();
-console.log('\n');
-console.log('Answer is', v1);
+bruteForce();
+
+function smartVersion()
+{
+    function process() 
+    {    
+        function extendDistribution(d) 
+        {
+            const r = [];
+            d.forEach((v, i) => {
+                r[i]      = (r[i] || 0) + (v / 2);
+                r[i + 1]  = (r[i + 1] || 0) + (v / 2);
+            })
+            return r;
+        }
+        
+        let distribution = [1];
+        const lengths    = [];
+        
+        for (let i = 1; i <= 32; ++i) 
+        {
+            distribution = extendDistribution(distribution);
+            let c = 0;
+            for (let j = i; j > 0; --j) 
+            {
+                let d = distribution[j];
+                let t = i - j;
+                let l = lengths[t] || 0;
+                c += d * (l + 1);
+            }
+            lengths[i] = (c + distribution[0]) / (1 - distribution[0]);
+        }
+        return lengths[32].toFixed(10);
+    }
+
+    let result = process();
+    console.log('Answer is', result);
+}
+
+//smartVersion();
