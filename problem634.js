51ef975b5de17615d03c3b73f49fbdc5651e1850
Dominique NORMAND <dominique@normand.us>
2018-08-12 14:27:08 -0400

    WIP

--- problem634.js
+++ problem634.js
@@ -13,198 +13,258 @@
 const primeHelper = require('tools/primeHelper')();
 const assert = require('assert');
 
-const MAX = 3E6;
-const MAX_PRIME = MAX; // Math.floor(Math.sqrt(MAX));
+const MAX = 9E12;
+const MAX_PRIME = Math.floor(Math.sqrt(MAX));
 
 primeHelper.initialize(MAX_PRIME);
 
-const primes = primeHelper.allPrimes();
-
-function isValid(value)
+function makeCubes()
 {
-    for (let a = 2; ; a++)
-    {
-        let a2 = a*a;
-        let a3 = a*a2;
+    let cubes = {};
+    for (let i = 2; i < 10; i++)
+        cubes[i*i*i] = i;
 
-        if (a2*a3 > value)
-            break;
-        
-        if (a2 * a3 === value)
-            return true;
-
-        for (let b = a+1; ; b++)
-        {
-            let b2 = b*b;
-            let b3 = b*b2;
-            let done = true;
-
-            let v1 = a2 * b3;
-            let v2 = a3 * b2;
-
-            if (v1 === value || v2 === value)
-                return true;
-            if (v1 > value && v2 > value)
-                break;
-        }
-    }
-    return false;
+    return cubes;
 }
 
-function $isValid(value)
+function makeSquares()
 {
-    let n = value;
+    let squares = {};
+    for (let i = 2; i < 10; i++)
+        squares[i*i] = i;
 
-    if (primeHelper.isPrime(value))
-        return false;
-
-    let a = false;
-    let b = false;
-    let c = false;
-
-    for (let p of primes)
-    {
-        if (p > n)
-            break;
-        if (n % p === 0)
-        {
-            let c = 0;
-            while (n % p === 0)
-            {
-                n /= p;
-                c++;
-            }
-
-            if (c % 5 === 0)
-            {
-                a = b = true;
-            }
-            else if (c % 3 === 0)
-            {
-                b = true;
-                let c1 = 0;
-                while (c % 3 === 0)
-                {
-                    c1++;
-                    c /= 3;
-                }
-                if (c1 % 2 === 0) // || c % 2 === 0)
-                    a = true;
-            }
-            else if (c % 2 === 0)
-            {
-                a = true;
-
-                let c1 = 0;
-                while (c % 2 === 0)
-                {
-                    c1++;
-                    c /= 2;
-                }
-                if (c1 % 3 === 0) // || c % 3 === 0)
-                    b = true;
-            }
-            else
-            {
-                while (c >= 5)
-                {
-                    c -= 5;
-                    a = b = true;
-                }
-                while (c >= 3)
-                {
-                    c -= 3;
-                    b = true;
-                }
-                while (c >= 2)
-                {
-                    c -= 2;
-                    a = true;
-                }
-                if (c !== 0)
-                    return false;
-            }
-
-            if (n === 1)
-                break;
-
-            if (primeHelper.isPrime(n))
-                return false;            
-        }
-    }
-    return n === 1 && a && b;
+    return squares;
 }
+
+const primes  = primeHelper.allPrimes();
+const squares = makeSquares();
+const cubes   = makeCubes();
 
 function F(max)
 {
     let total = 0;
-    for (let n = 2; n <= max; n++)
-    {
-        let v1 = isValid(n);
+    let factors = [];
 
-        if (v1)
+    function $isValid(value)
+    {
+        for (let a = 2; ; a++)
         {
-            console.log(v1);
+            let a2 = a*a;
+            let a3 = a*a2;
+    
+            if (a2*a3 > value)
+                break;
+            
+            if (a2 * a3 === value)
+            {
+                console.log(value, ' => ', a , '^5');
+                return true;
+            }
+
+            for (let b = a+1; ; b++)
+            {
+                let b2 = b*b;
+                let b3 = b*b2;
+    
+                let v1 = a2 * b3;
+                let v2 = a3 * b2;
+    
+                if (v1 === value)
+                {
+                    console.log(value, ' => ', a , '^2 * ', b, '^3');
+                    return true;
+                }
+                if (v2 === value)
+                {
+                    console.log(value, ' => ', a , '^3 * ', b, '^2');
+                    return true;
+                }
+                if (v1 > value && v2 > value)
+                    break;
+            }
+        }
+        return false;
+    }
+
+    function isValid(value)
+    {       
+        let square = false;
+        let cube   = false;
+        let either = false;
+        let bad    = false;
+
+        function checkFactor(f)
+        {
+            // if (cubes[f] !== undefined)
+            // {
+            //     cube = true;
+            //     checkFactor(cubes[f]);
+            //     return;
+            // }
+            // if (squares[f] !== undefined)
+            // {
+            //     square = true;
+            //     checkFactor(squares[f]);
+            //     return;
+            // }
+
+            if (f % 2 === 0 && f % 3 === 0)
+            {
+                either = true;
+            }
+            else if (f % 5 === 0)
+            {
+                cube = square = true;
+            }
+            else if (f % 3 === 0)
+            {
+                cube = true;
+                if (f !== 3 && ! square)
+                {   
+                    let x = f;
+                    while (x % 3 === 0)
+                    {
+                        if (squares[x] !== undefined)
+                        {
+                            square = true;
+                            break;
+                        }
+                        x /= 3;
+                    }
+                    if (! square)
+                        checkFactor(f / 3);
+                }
+            }
+            else if (f % 2 === 0)
+            {
+                square = true;
+                if (f !== 2 && ! cube)
+                {                    
+                    let x = f;
+                    while (x % 2 === 0)
+                    {
+                        if (cubes[x] !== undefined)
+                        {
+                            cube = true;
+                            break;
+                        }
+                        x /= 2;
+                    }
+                    if (! cube)
+                        checkFactor(f / 2);
+                }
+            }
+            else   
+            {              
+                let c = { c:cube, s:square, e:either };
+                
+                if (f > 3)
+                {
+                    square = false;
+                    cube   = false;
+                    either = false;
+
+                    checkFactor(f - 3);
+
+                    if (!bad && (either || square))
+                    {
+                        cube    = true;
+                        square |= c.square;
+                        either |= c.either;
+                        return;
+                    }
+
+                    square = false;
+                    cube   = false;
+                    either = false;
+                    bad    = false;
+
+                    checkFactor(f - 2);
+
+                    if (!bad && (either || cube))
+                    {
+                        square  = true;
+                        cube   |= c.cube;
+                        either |= c.either;
+                        return;
+                    }
+
+                    square = c.square;
+                    cube   = c.cube;
+                    either = c.either;
+                    bad    = true;
+                }
+            }
+        }
+
+        for (let f of factors)
+        {            
+            checkFactor(f);
+            if (bad)
+            {
+                if ($isValid(value))
+                    return true;
+                return false;
+            }
+        }
+
+        if (square && cube)
+            return true;
+        if (either)
+            return square || cube;
+
+        if ($isValid(value))
+            return true;
+        return false;
+    }
+
+    function inner(value, index)
+    {
+        if (value > max)
+            return;
+
+        if (isValid(value))
+        {
             total++;
         }
-    }   
-    return total;
-}
 
-function $F(max)
-{
-    let total = 0;
-    
-    for (let i = 0; i < primes.length; i++)
-    {
-        let a = primes[i];
-
-        let a2 = a*a;
-        if (a2 > max)
-            break;
-
-        let a3 = a2*a;
-
-        if (a2 * a3 <= max)
-            total++;
-        else
-            break;
-
-        for (let j = i+1; j < primes.length; j++)
+        for (let idx = index; idx < primes.length; idx++)
         {
-            let b = primes[j];
-            let b2 = b * b;
-            let b3 = b * b2;
+            let p = primes[idx];
+            let p2 = p*p;
 
-            let done = true;
-            
-            if (a2 * b3 <= max)
-            {
-                done = false;
-                total++;
-            }
-            if (a3 * b2 <= max)
-            {
-                done = false;
-                total++;
-            }
-            if (done)
+            let v = value * p2;
+            if (v > max)
                 break;
+
+            let i = factors.length;
+            let f = 2;
+
+            factors.push(2);
+
+            while (v <= max)
+            {
+                inner(v, idx+1);
+                v *= p;
+                factors[i]++;
+            }
+            factors.pop();
         }
     }
 
+    // factors.push(16);
+    // isValid(Math.pow(2, 16));
+    // factor.pop();
+
+    inner(1, 0);
+
     return total;
 }
 
-// assert.equal(isValid(576), isValid2(576));
-// assert.equal(isValid(64),  isValid2(64));
-// assert.equal(isValid(128), true);
-// assert.equal(isValid(432), true);
-// assert.equal(isValid(256), true);
-// assert.equal(isValid(1728), isValid2(1728));
+//assert.equal(F(100), 2);
+// assert.equal(F(2E4), 130);
+assert.equal(F(3E6), 2014);
 
-// assert.equal(F(100), 2);
-assert.equal(F(2E4), 130);
-// assert.equal(F(3E6), 2014);
+// let answer = F(9E18);
 
 console.log('Done');
