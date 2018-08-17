f82816f7aa7a7d5f275bbfabc33669813e77c890
Dominique NORMAND <dominique@normand.us>
2018-08-16 22:38:19 -0400

    WIP

--- problem396.js
+++ problem396.js
@@ -25,17 +25,83 @@
 const assert = require('assert');
 const bigInt = require('big-integer');
 
+const MODULO = 1000000000;
+
 function G(n)
+{
+    let k = 2;
+    let digits = [];
+
+    while (n > 0)
+    {
+        let d = n % 2;
+        digits.push(d);
+        n = (n - d) / 2;
+    }
+
+    if (digits.length === 0)
+        return 0;
+    if (digits.length > 4)
+        throw "Too big";
+
+    while(true)
+    {
+        let d = digits[0];
+
+        if (d > 0)
+        {
+            k = (k + d) % MODULO;
+            if (digits.length === 1)
+                return k;
+        }
+
+        d = digits[1];
+
+        if (d > 0)
+        {
+            let off = bigInt(2).modPow(d, MODULO).prev().times(k+1).mod(MODULO).valueOf();
+            k = (k + off + 1) % MODULO;
+
+            if (digits.length === 2)
+                return k-3;
+        }
+        else if (digits.length === 2)
+            throw "How can the last bit be zero?";
+        else if (digits.length === 3)
+            k = (k + 1) % MODULO;
+
+        d = digits[2];
+        if (d > 0)
+        {
+            digits[0] = k-1;
+            digits[1] = k-1;
+            digits[2]--;
+            if (digits[2] === 0 && digits.length === 3)
+                digits.pop();
+            continue;
+        }
+        else if (digits.length === 3)
+            throw "How can the last bit be zero?";
+
+        k = (k + 1) % MODULO;
+
+        digits[0] = k-1;
+        digits[1] = k-1;
+        digits[2] = k-1;
+        digits[3]--;
+        if (digits[3] === 0 && digits.length === 4)
+            digits.pop();
+    }
+}
+
+function $G(n)
 {
     let g = bigInt(n);
     let k = bigInt(2);
-    let count = 0
     let digits = [];
 
     while (g.gt(0))
     {
-        count = (count+1) % 1000000000;
-
         while (g.gt(0))
         {
             let d = g.mod(k);
@@ -43,22 +109,67 @@ function G(n)
             g = g.minus(d).divide(k);
         }
 
-        k = k.next();
+        let d0 = digits[0];
+
+        if (d0.greater(0))
+        {
+            let d1 = digits[1] || bigInt.zero;
+
+            k = k.plus(d0).next();
+
+            // let c1 = (count + d0.mod(MODULO).valueOf()) % MODULO;
+
+            if (! d1.greater(0) && digits.length <= 2)
+            {
+                count = d0.plus(count).prev().mod(MODULO).valueOf();
+                console.log(count, c1);
+                return count
+            }
+
+            if (digits.length <= 2)
+            {
+                let kk  = k.mod(MODULO).valueOf();
+                let off = bigInt(2).modPow(d1, MODULO).prev().times(kk).mod(MODULO).valueOf();
+                kk    = (kk + off) % MODULO;
+                return kk - 3; // count;
+            }
+            else if (d1.greater(0))
+            {
+                let off = bigInt(2).pow(d1).prev().times(k);
+                k       = k.plus(off);
+            }
+
+            digits[0] = bigInt.zero;
+            digits[1] = bigInt.zero;
+        }
+        else
+            k = k.next();
 
         while (digits.length > 0)
             g = g.times(k).plus(digits.pop());
 
         g = g.prev();
     }
-    return count;
+    return k.minus(3).mod(MODULO);
 }
 
-// assert.equal(G(2), 3);
-// assert.equal(G(4), 21);
-// assert.equal(G(6), 381);
+assert.equal(G(2), 3);
+assert.equal(G(4), 21);
+assert.equal(G(6), 381);
+assert.equal(G(7), 2045);
+assert.equal(G(8), 722374141);
 
-for (let i = 1; i < 9/*16*/; i++)
-{
-    console.log(i + " => " + G(i));
-}
+console.log("G(9) =", G(9));
+console.log("G(10) =", G(10));
+console.log("G(11) =", G(11));
+console.log("G(12) =", G(12));
+console.log("G(13) =", G(13));
+console.log("G(14) =", G(14));
+console.log("G(15) =", G(15));
+
+// for (let i = 1; i < 9/*16*/; i++)
+// {
+//     console.log(i, "=>", G(i));
+// }
+
 console.log("Done");
