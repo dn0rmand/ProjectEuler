5422d76705eedea0dcb0ab46649e3887597177d8
Dominique NORMAND <dominique@normand.us>
2018-10-10 20:40:46 -0400

    WIP

--- problem635.js
+++ problem635.js
@@ -13,252 +13,161 @@
 
 // Find S2(10^8)+S3(10^8). Give your answer modulo 1000000009
 
+
+// Aq(n)=((q*n C n) + ((−1)^(n−1))*q*(n-1)) / n
+
 const assert = require('assert');
 const primeHelper = require('tools/primeHelper')();
+
 const MODULO = 1000000009;
+const MODULO_N = BigInt(MODULO);
 
-primeHelper.initialize(1E6);
+const MAX = 100000000;
 
-class SubsetCounter
+const factorial = new Uint32Array(3*MAX);
+
+function preload()
 {
-    constructor(q, n)
-    {
-        if (q !== 2 && q !== 3)
-            throw "Not supported";
+    console.log("Preloading primes")
+    primeHelper.initialize(MAX, true);
 
-        this.q = q;
-        this.n = n;
-        this.memoize = new Map();
-    }
+    console.log("Preloading factorials")
+    let value = 1;
+    let x     = 2;
+    let max   = 3*MAX;
 
-    get(count, max, value)
+    while (x <= max)
     {
-        let m = this.memoize.get(value);
-        if (m !== undefined)
-            m = m.get(count);
-        if (m !== undefined)
-            return m.get(max);
-    }
-
-    set(count, max, value, subSets)
-    {
-        let m = this.memoize.get(value);
-        if (m === undefined)
+        let v = (value * x);
+        if (v > Number.MAX_SAFE_INTEGER)
         {
-            m = new Map();
-            this.memoize.set(value, m);
+            v = (BigInt(value) * BigInt(x)) % MODULO_N;
+            value = Number(v);
         }
-        let c = m.get(count);
-        if (c === undefined)
+        else
         {
-            c = new Map();
-            m.set(count, c);
+            value = v % MODULO;
         }
-        c.set(max, subSets);
+        factorial[x++] = value;
     }
 
-    doCount()
-    {
-    }
-
-    countSubSets(modulo, max, count)
-    {
-        if (count === 0)
-        {
-            if (modulo === 0)
-            {
-                return 1;
-            }
-            else
-                return 0;
-        }
-        else if (count < 0)
-            return 0;
-
-        let total = this.get(count, max, modulo);
-        if (total !== undefined)
-            return total;
-
-        total = 0;
-
-        // check
-
-        try
-        {
-            for (let i = max; i >= 0; i--)
-            {
-                let result1, result2, result3;
-
-                // use once ( example 1 or 1+n ... 1+(q-1)*n)
-                let v = (modulo + i) % this.n;
-
-                result1 = this.countSubSets(v, i-1, count-1);
-                total = (total + (this.q*result1))// % MODULO;
-
-                if (count > 1)
-                {
-                    // used both ( 1 and 1+n )
-                    v = (modulo + (i*2)) % this.n;
-                    result2 = this.countSubSets(v, i-1, count-2);
-                    if (this.q === 2)
-                        total = (total + result2)// % MODULO;
-                    else if (this.q === 3)
-                        total = (total + (3*result2))// % MODULO;
-                }
-                if (count > 2 && this.q === 3)
-                {
-                    v = (modulo + (i*3)) % this.n;
-                    result3 = this.countSubSets(v, i-1, count-3);
-                    total = (total + result3)// % MODULO;
-                }
-            }
-
-            return total;
-        }
-        finally
-        {
-            this.set(count, max, modulo, total);
-        }
-    }
-
-    count()
-    {
-        this.doCount();
-
-        return this.countSubSets(0, this.n-1, this.n);
-    }
+    console.log('Preloaded')
 }
 
-function S(q, L)
+function modInv(value, n)
+{
+    let t    = 0n;
+    let newT = 1n;
+    let r    = BigInt(n);
+    let newR = BigInt(value);
+    let q, lastT, lastR;
+
+    if (newR < 0)
+        newR = -newR;
+
+    while (newR != 0)
+    {
+        q = r / newR;
+        lastT = t;
+        lastR = r;
+        t = newT;
+        r = newR;
+        newT = lastT - q * newT;
+        newR = lastR - q * newR;
+    }
+    if (r != 1)
+        throw new Error(value + " and " + n + " are not co-prime");
+
+    if (t < 0)
+        t += BigInt(n);
+
+    if (n < 0)
+        return -t;
+    return t;
+}
+
+function divise(quotient, divisor)
+{
+    divisor = modInv(divisor, MODULO);
+
+    return Number( (divisor * BigInt(quotient)) % MODULO_N );
+}
+
+function CNR(n, r)
+{
+    let N = factorial[n];
+    let R = factorial[r];
+    let NR= factorial[n-r];
+
+    let divisor = (BigInt(R) * BigInt(NR)) % MODULO_N;
+    let result  = divise(N, Number(divisor));
+
+    return result;
+}
+
+function A(q, n)
+{
+    if (q !== 2 && q !== 3)
+        throw "q can only be 2 or 3 supported"
+
+    // Special case
+    if (n === 2)
+        return q === 2 ? 2 : 6;
+
+    // Normal case
+
+    let v1 = q*(n-1);
+    let v2 = CNR(q*n, n);
+
+    let result = divise((v1 + v2) % MODULO, n) ;
+    return result;
+}
+
+function S(q, n)
 {
     let total = 0;
-
-    for(let prime of primeHelper.primes())
+    for (let p of primeHelper.primes())
     {
-        if (prime > L)
+        if (p > n)
             break;
 
-        let v = new SubsetCounter(q, prime).count();
+        total = (total + A(q, p)) % MODULO;
+    }
+    return total;
+}
 
-        total = (total + v) % MODULO;
+function Solve(n)
+{
+    let total = 0;
+    for (let p of primeHelper.primes())
+    {
+        if (p > n)
+            break;
+
+        let a = (A(2, p) + A(3, p)) % MODULO;
+
+        total = (total + a) % MODULO;
     }
 
     return total;
 }
 
-function A(q, n)
-{
-    return new SubsetCounter(q, n).count();
-}
-
-function AA(q, n)
-{
-    let k = n;
-    let ncnt= []
-    for (let x = 1; x <= q*n; x++)
-    {
-        let i = x % k;
-        ncnt[i] = (ncnt[i] || 0)+1;
-    }
-
-    function waysToCreate(input_class, class_idx, n)
-    {
-        let ways = 0
-        // not using this class:
-        if (class_idx+1 < k)
-            ways += waysToCreate(input_class, class_idx+1, n);
-
-        for(let i = 1; i < ncnt[class_idx] && i <= n; )
-        {
-            let new_input_class = (input_class + i*class_idx) % k;
-
-            if (i == n && new_input_class != 0)
-            {
-                break; // all elements are used, but doesn't congrunent with 0 (mod k)
-            }
-
-            let subways  =1;
-            if (class_idx+1 < k)
-                subways = waysToCreate(new_input_class, class_idx+1, n-i)
-
-            ways += nchoosek(ncnt[class_idx], i) * subways;
-        }
-
-        return ways;
-    }
-
-    
-}
-
-function subCount(q, n)
-{
-    let k = n;
-
-    n = n*q;
-
-    let mod = [];
-    for(i = 0; i < k; i++)
-        mod.push(0);
-
-    let cumSum = 0;
-    for (let i = 0; i < n; i++)
-    {
-        cumSum += i+1;
-        mod[cumSum % k]++;
-    }
-
-    // Initialize result
-    let result = 0;
-
-    // Traverse mod[]
-    for (let i = 0; i < k; i++)
-    {
-        // If there are more than one prefix subarrays
-        // with a particular mod value.
-        if (mod[i] > 1)
-            result += (mod[i] * (mod[i] - 1)) / 2;
-    }
-    result += mod[0];
-
-    return result;
-}
-
-// console.log(A(2,1));
-// console.log(A(2,2));
-// console.log(A(2,3));
-// console.log(A(2,4));
-// console.log(A(2,5));
-// console.log(A(2,6));
-// console.log(A(2,7));
-
-console.log(subCount(2, 5));
+console.time(635);
+preload();
 
 assert.equal(A(2,5), 52);
 assert.equal(A(3,5), 603);
 
-assert.equal(A(2,7), 492);
-assert.equal(A(3,7), 16614);
+assert.equal(A(2, 7), 492);
+assert.equal(A(3, 7), 16614);
 
-assert.equal(S(2,10), 554);
-assert.equal(A(3,10), 3004206);
-
-let total = 0;
-for (let i = 2; i < 36; i++)
-{
-    let v1 = A(2, i);
-    console.log("A(2,", i, ") =", v1, "- total", total);
-    total += v1;
-}
-
-// console.time("A2(100)")
-// assert.equal(A(2,100), 52431190);
-// console.timeEnd("A2(100)")
-
-// console.time("S2(100)")
-// assert.equal(S(2,100), 100433628);
-// console.timeEnd("S2(100)")
-// console.time("S3(100)")
-// assert.equal(S(3,100), 855618282);
-// console.timeEnd("S3(100)")
+assert.equal(S(2, 10), 554);
+assert.equal(S(2, 100), 100433628);
+assert.equal(S(3, 100), 855618282);
 
 console.log("All tests passed");
+
+let answer = Solve(MAX);
+
+console.timeEnd(635);
+console.log("Answer is", answer);
