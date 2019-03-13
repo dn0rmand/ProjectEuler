57ad61d2478099e40b2e5021f5c9bcb00c0baee8
Dominique NORMAND <dominique@normand.us>
2019-03-13 19:18:45 -0400

    203 done

---
+++ Solved/problem203.js
@@ -0,0 +1,59 @@
+const primeHelper = require('tools/primeHelper')();
+const assert = require('assert');
+
+function nextRow(previousRow)
+{
+    let currentRow = [];
+    let length = previousRow.length+1;
+
+    currentRow[0] = 1;
+    currentRow[length-1] = 1;
+    for (let i = 1; i < length/2; i++)
+    {
+        let value = previousRow[i-1] + previousRow[i];
+        currentRow[i] = currentRow[length-(i+1)] = value;
+    }
+    return currentRow;
+}
+
+function solve(maxRow)
+{
+    let previous = [];
+    let distinctValues = new Set();
+
+    for(let i = 1; i <= maxRow; i++)
+    {
+        let r = nextRow(previous);
+        r.forEach((v) => {
+            distinctValues.add(v);
+        });
+        previous = r;
+    }
+
+    let total = 0;
+    for (let v of distinctValues)
+    {
+        let squareFree = true;
+        primeHelper.factorize(v, (p, c) => {
+            if (c > 1)
+            {
+                squareFree = false;
+                return false;
+            }
+        });
+        if (squareFree)
+            total += v;
+    }
+
+    return total;
+}
+
+console.time(203);
+
+primeHelper.initialize(2E7);
+assert.equal(solve(8), 105);
+
+let answer = solve(51);
+
+console.timeEnd(203);
+console.log("Answer is", answer);
