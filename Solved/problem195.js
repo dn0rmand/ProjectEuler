d66ed08ccd936abf74fa055c9cbd031f875468d3
Dominique NORMAND <dominique@normand.us>
2019-03-16 17:21:52 -0400

    195 done

---
+++ Solved/problem195.js
@@ -0,0 +1,152 @@
+const assert = require('assert');
+const prettyTime= require("pretty-hrtime");
+
+require('tools/numberHelper');
+
+const RADIUS = 1053779;
+
+function gcd(a, b)
+{
+    while (b !== 0)
+    {
+        let c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+};
+
+function T(radius, trace)
+{
+    function getRadius(a, b, c)
+    {
+        var s = (a + b + c) / 2;
+        var r = Math.sqrt(s) * Math.sqrt(s - a) * Math.sqrt(s - b) * Math.sqrt(s - c)
+        r /= s;
+
+        return r;
+    }
+
+    function triangles(callback)
+    {
+        let a, b, c;
+
+        let generate = (m, n) => {
+            let n2 = n * n;
+            let m2 = m * m;
+            let mn = m * n;
+
+            a = m2 - mn + n2 ;
+            b = 2*mn - n2;
+            c = m2 - n2;
+
+            if ((m+n) % 3 === 0)
+            {
+                a /= 3;
+                b /= 3;
+                c /= 3;
+            }
+        };
+
+        let missed = 0;
+
+        for (let n = 1; ; n++)
+        {
+            if (trace)
+                process.stdout.write(`\r${n}`);
+
+            let done = true;
+
+            for (let m = n*2+1; ; m++)
+            {
+                if ((m+n) % 3 === 0)
+                    continue;
+
+                if (gcd(m, n) === 1)
+                {
+                    generate(m, n);
+
+                    let r = getRadius(a, b, c);
+
+                    if (r > radius)
+                        break;
+
+                    done = false;
+                    callback(a, b, c);
+                }
+            }
+
+            if (n % 3 !== 0)
+            {
+                let start = n*2+1;
+                while ((start+n) % 3 !== 0)
+                    start++;
+                for (let m = start; ; m += 3)
+                {
+                    if (gcd(m, n) === 1)
+                    {
+                        generate(m, n);
+
+                        let r = getRadius(a, b, c);
+
+                        if (r > radius)
+                            break;
+
+                        done = false;
+                        callback(a, b, c);
+                    }
+                }
+            }
+
+            if (done)
+            {
+                missed++;
+                if (missed ===3)
+                    break;
+            }
+            else
+                missed = 0;
+        }
+    }
+
+    let total = 0;
+
+    triangles((a, b, c) =>
+    {
+        let a1 = 0;
+        let b1 = 0;
+        let c1 = 0;
+        let f  = 0;
+        let r  = getRadius(a, b, c);
+
+        while (true)
+        {
+            f++;
+
+            a1 += a;
+            b1 += b;
+            c1 += c;
+
+            let r1 = getRadius(a1, b1, c1);
+
+            if (r1 > radius)
+                break;
+
+            total++;
+        }
+    });
+
+    if (trace)
+        console.log('');
+
+    return total;
+}
+
+assert.equal(T(100), 1234);
+assert.equal(T(1000), 22767);
+assert.equal(T(10000), 359912);
+
+let timer = process.hrtime();
+let answer = T(RADIUS, true);
+timer = process.hrtime(timer);
+console.log('Answer is', answer, 'executed in', prettyTime(timer, {verbose:true}));
