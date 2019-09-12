beaad73caffd3cc9d8a89188c9812455da88dc8b
Dominique NORMAND <dominique@normand.us>
2019-09-11 21:06:55 -0400

    Simplified Running out of ideas

--- problem678.js
+++ problem678.js
@@ -2,9 +2,6 @@ const assert = require('assert');
 const timeLog = require('tools/timeLogger');
 const MAX = 10 ** 11;
 
-if (MAX > Number.MAX_SAFE_INTEGER)
-    throw "TOO BIG";
-
 function biggy(n)
 {
     if (MAX > Number.MAX_SAFE_INTEGER)
@@ -19,16 +16,10 @@ const TWO  = biggy(2);
 
 const FACTOR_2 = ONE;
 
-const TRACE_SPEED = 10000;
-
-const $digitCount = new Map();
+const TRACE_SPEED = 1000;
 
 function digitCounts(value)
 {
-    let count = $digitCount.get(value);
-    if (count !== undefined)
-        return count;
-
     let v = value;
 
     count = 0;
@@ -39,7 +30,6 @@ function digitCounts(value)
 
         v >>= ONE;
     }
-    $digitCount.set(value, count);
     return count;
 }
 
@@ -103,8 +93,6 @@ function F(N, trace)
     let maxValue = values[values.length-1];
     let traceCount = 0;
 
-    let MAXFLAG = 2n**60n;
-
     for(let C of values)
     {
         if (trace)
@@ -118,7 +106,7 @@ function F(N, trace)
         let pc  = map.get(C);
 
         if (C === ONE || pc <= 1n)
-            continue; // Not a valide c^f
+            continue; // Not a valid c^f
 
         pc = (pc | FACTOR_2) - FACTOR_2; // remove factor 2
         if (! pc)
@@ -141,10 +129,7 @@ function F(N, trace)
             if (! pab) // no common factors
                 continue;
 
-            let pabc = pab & pc; // count to remove
-            let count = digitCounts(pab) * countC;
-            
-            subCount += count - digitCounts(pabc);
+            subCount+= digitCounts(pab) * countC;
         }
 
         if (subCount != 0)
@@ -152,21 +137,73 @@ function F(N, trace)
     }
 
     if (trace)
-        console.log('\r');
+        process.stdout.write('\r           \r');
 
     return total;
 }
 
-assert.equal(F(1E3), 7);
-assert.equal(F(1E5), 53);
-assert.equal(F(1E7), 287);
-assert.equal(F(1E9), 1429);
-// assert.equal(F(1E10), 3231);
+function analyze()
+{
+    console.log(F(1E3));
+    console.log(F(1E4));
+    console.log(F(1E5));
+    console.log(F(1E6));
+    console.log(F(1E7));
+    console.log(F(1E8));
+    console.log(F(1E9));
+}
 
-// 1E12 -> 16066
-console.log('Tests passed');
+function runTests()
+{
+    assert.equal(F(1E3), 7);
+    assert.equal(F(1E5), 53);
+    assert.equal(F(1E7), 287);
+    assert.equal(F(1E9), 1429);
+    assert.equal(F(1E10), 3231);
 
-let answer = timeLog.wrap("solving 678\n", () => {
+    // 1E12 -> 16066
+    console.log('Tests passed');
+}
+
+// analyze();
+runTests();
+
+let answer = timeLog.wrap("", () => {
     return F(MAX, true);
 });
 console.log('Answer is', answer);
+
+/*
+
+ 2^2 + 11^2 =  5^3 - 1
+ 5^2 + 10^2 =  5^3 - 1
+ 3^3 +  6^3 =  3^5 - 1
+ 7^2 + 24^2 =  5^4 - 1
+15^2 + 20^2 =  5^4 - 1
+10^2 + 30^2 = 10^3 - 1
+18^2 + 26^2 = 10^3 - 1
+ 9^2 + 46^2 = 13^3 - 1
+26^2 + 39^2 = 13^3 - 1
+10^2 + 55^2 =  5^5 - 1
+25^2 + 50^2 =  5^5 - 1
+38^2 + 41^2 =  5^5 - 1
+17^2 + 68^2 = 17^3 - 1
+47^2 + 52^2 = 17^3 - 1
+
+ 9^3 + 18^3 =  9^4
+ 9^3 + 18^3 =  3^8
+
+16^2 + 88^2 = 20^3 - 1
+40^2 + 80^2 = 20^3 - 1
+28^2 + 96^2 = 10^4 - 1
+60^2 + 80^2 = 10^4 - 1
+
+
+35^2 + 120^2 = 25^3
+             = 5^6 - 2
+
+
+1936 + 13689 = 15625 - 2 ( 5^6 or 25^3)
+5625 + 10000 = 15625 - 2
+
+*/
