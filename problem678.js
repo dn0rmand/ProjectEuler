9ca236e90add3c45a3be268492cf39560bbb5585
Dominique NORMAND <dominique@normand.us>
2019-09-17 18:03:13 -0400

    wip

--- problem678.js
+++ problem678.js
@@ -2,228 +2,131 @@ const assert      = require('assert');
 const timeLog     = require('tools/timeLogger');
 const primeHelper = require('tools/primeHelper')();
 
-const MAX = 10n ** 12n;
-const MAX_PRIME = 1E6;
+const MAX = 10n ** 14n;
 
-primeHelper.initialize(MAX_PRIME, true);
-
+primeHelper.initialize(1E7, true);
 const allPrimes = primeHelper.allPrimes();
 
-const ZERO = 0n;
-const ONE  = 1n;
-const TWO  = 2n;
-const THREE= 3n;
-const FOUR = 4n;
+const ZERO  = 0n;
+const ONE   = 1n;
+const TWO   = 2n;
+const THREE = 3n;
 
 const TRACE_SPEED = 1000;
 
-function digitCounts(value)
+function setToArray(set)
 {
-    let v = value;
-
-    count = 0;
-    while (v !== ZERO)
-    {
-        if (v & ONE)
-            count++;
-
-        v >>= ONE;
-    }
-    return count;
-}
-
-function buildMap(max, trace)
-{
-    max = BigInt(max);
-
-    let map = new Map();
-
-    let values = [];
-
-    let traceCount = 0;
-
-    for (let a = TWO; ; a++)
-    {
-        if (trace)
-        {
-            if (traceCount === 0)
-                process.stdout.write(`\r${ a }`);
-            if (++traceCount >= TRACE_SPEED)
-                traceCount = 0;
-        }
-        let v = a*a*a; // a ^ 3
-        if (v > max)
-            break;
-
-        for (let flag = ONE; v <= max ; flag <<= ONE, v *= a)
-        {
-            let m = map.get(v);
-            if (m === undefined)
-            {
-                m = flag;
-                values.push(v);
-            }
-            else
-                m |= flag;
-
-            map.set(v, m);
-        }
-    }
-
-    if (trace)
-        process.stdout.write(`\r                                   \r`);
-
+    values = [...set];
     values.sort((a, b) => {
         if (a < b)
             return -1;
-        else if (a === b)
-            return 0;
-        else
+        if (a > b)
             return 1;
-    });
-
-    return [values, map];
+        return 0;
+    })
+    return values;
 }
 
-function factorize(n, callback)
+function loadValues(max)
 {
-    var max = MAX_PRIME;
-    if (n < Number.MAX_SAFE_INTEGER)
+    max = BigInt(max);
+    let values = new Set();
+    for(let p = 3n; 2n ** p <= max; p++)
     {
-        let s = Math.round(Math.sqrt(Number(n)))+1;
-        max = Math.max(max, s);
-    }
-
-    for (let p of allPrimes)
-    {
-        if (p > max)
-            break;
-        p = BigInt(p);
-        if (p > n)
-            break;
-        let f = 0;
-
-        while ((n % p) === ZERO)
+        for (let v = 2n; ; v++)
         {
-            n /= p;
-            f++;
-        }
-        if (f)
-        {
-            if (callback(p, f) === false)
-                return;
+            let vv = v ** p;
+            if (vv > max)
+                break;
+            values.add(vv);
         }
     }
-    if (n !== ONE)
-        callback(n, 1);
+    values = setToArray(values);
+    return values;
 }
 
-function getSquareSumCount(C)
+function loadPowers(max, power)
 {
-    let count = 1;
+    power = BigInt(power);
+    max   = BigInt(max);
 
-    if (C > Number.MAX_SAFE_INTEGER)
+    let map = new Set();
+
+    for (let a = 3n; ; a++)
     {
-        factorize(C, (p, f) => {
+        let A = a ** power;
+        if (A > max)
+            break;
 
-            if ((f & 1) && (p % FOUR) === THREE)
-            {
-                count = 0;
-                return false;
-            }
-            if ((p % FOUR) === ONE)
-            {
-                count *= (f+1);
-            }
-        });
+        map.add(A);
     }
-    else
-    {
-        primeHelper.factorize(Number(C), (p, f) => {
-            if ((f & 1) && (p % 4) === 3)
-            {
-                count = 0;
-                return false;
-            }
-            if ((p % 4) === 1)
-            {
-                count *= (f+1);
-            }
-        });
-    }
-    
-    if (count & 1)
-        count = (count-1)/2;
-    else
-        count = count/2;
-    // count >>= 1;
+    if (map.size === 0)
+        return undefined;
 
-    return count;
+    return [setToArray(map), map];
 }
 
 function F(N, trace)
 {
-    let total   = 0;
-    let [values, map] = buildMap(N, trace);
-    let maxValue = values[values.length-1];
-    let traceCount = 0;
+    N = BigInt(N);
+    let total = 0;
 
-    for(let C of values)
-    {
-        if (trace)
-        {
-            if (traceCount === 0)
-                process.stdout.write(`\r${ maxValue - C }    `);
-            if (++traceCount >= TRACE_SPEED)
-                traceCount = 0;
-        }
-
-        let pc  = map.get(C);
-
-        if (C === ONE || pc < ONE)
-            continue; // Not a valid c^f
-
-        let cc = getSquareSumCount(C);
-        if (cc != 0)
-        {
-            console.log(C,'->',cc);
-            total += cc;
-        }
-        let countC = digitCounts(pc);
-
-        if (! countC)
-            continue;
-
-        for (let A of values)
-        {
-            let B = C - A;
-            if (B <= A)
-                break;
-
-            let pa = map.get(A) || ZERO;
-            let pb = map.get(B) || ZERO;
-            let pab = pa & pb;
-            if (pab)
-            	total += digitCounts(pab) * countC;
-        }
-    }
+    let maps  = new Map();
 
     if (trace)
-        process.stdout.write('\r           \r');
+        process.stdout.write('Loading powers ..');
+
+    let values = loadValues(N);
+
+    for (let pa = 2; ; pa++)
+    {
+        let map = loadPowers(N, pa);
+        if (! map)
+            break;
+        maps.set(pa, map);
+    }
+    if (trace)
+        console.log('.. Done');
+
+    for (let C of values)
+    {
+        if (trace)
+            process.stdout.write(`\rprocessing ${C}`);
+
+        // calculate count for powers of 2
+
+        for (let pa = 2; ; pa++)
+        {
+            let As = maps.get(pa);
+            if (! As)
+                break;
+
+            let [vals, map] = As;
+            for (let A of vals)
+            {
+                let B = C-A;
+                if (B < A)
+                    break;
+                if (map.has(B))
+                    total++;
+            }
+        }
+    }
 
     return total;
 }
 
 function analyze()
 {
+    console.log(F(1E3));
     console.log(F(1E11));
-    // console.log(F(1E3));
-    // console.log(F(1E4));
-    // console.log(F(1E5));
-    // console.log(F(1E6));
-    // console.log(F(1E7));
-    // console.log(F(1E8));
-    // console.log(F(1E9));
+
+    console.log(F(1E4));
+    console.log(F(1E5));
+    console.log(F(1E6));
+    console.log(F(1E7));
+    console.log(F(1E8));
+    console.log(F(1E9));
 }
 
 function runTests()
@@ -238,7 +141,7 @@ function runTests()
     console.log('Tests passed');
 }
 
-// analyze();
+analyze();
 runTests();
 
 let answer = timeLog.wrap("", () => {
