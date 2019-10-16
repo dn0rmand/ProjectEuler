41342784e1172dd71e58a39b6df280b18934efe2
Dominique NORMAND <dominique@normand.us>
2019-10-15 21:01:10 -0400

    227

--- problem227.js
+++ problem227.js
@@ -1,108 +1,74 @@
-const assert = require('assert');
 const timeLog = require('tools/timeLogger');
+const DELTA   = 1E-11;
 
-const PLAYERS = 10;
-
-class State
+function solve(PLAYERS)
 {
-    constructor(dice1, dice2, probability)
-    {
-        if (dice1 < 0)
-            dice1 += PLAYERS;
-        if (dice2 < 0)
-            dice2 += PLAYERS;
+    const MIDDLE = Math.floor(PLAYERS/2);
 
-        this.dice1 = dice1 % PLAYERS;
-        this.dice2 = dice2 % PLAYERS;
-        this.probability = probability;
-    }
-
-    createNext(v1, v2, probability)
-    {
-        let s = new State(this.dice1+v1, this.dice2+v2, this.probability * probability);
-
-        return s;
-    }
-
-    get key()
-    {
-        return this.dice1 * (PLAYERS*10) + this.dice2;
-    }
-
-    get loser()
-    {
-        return this.dice1 == this.dice2;
-    }
-}
-
-class StateCollection
-{
-    constructor()
-    {
-        this.inner = new Map();
-    }
-
-    add(state)
-    {
-        let k = state.key;
-        let s = this.inner.get(k);
-        if (s)
-            s.probability += state.probability;
-        else
-            this.inner.set(k, state);
-    }
-
-    forEach(callback)
-    {
-        for (let state of this.values)
-            callback(state);
-    }
-
-    get values() { return this.inner.values(); }
-
-    get size() { return this.inner.size; }
-}
-
-function solve()
-{
     let result = 0;
-    let states = new StateCollection();
+    let states = new Float64Array(MIDDLE+1).fill(0);
+    let newStates = new Float64Array(MIDDLE+1);
 
-    let middle = Math.floor(PLAYERS / 2);
-    states.add(new State(1, middle, 1));
+    states[MIDDLE] = 1;
 
-    for(let turn = 0; turn < 1500; turn++)
+    const PLUS1 = 8/36;
+    const PLUS2 = 1/36;
+    const MINUS1= 8/36;
+    const MINUS2= 1/36;
+    const SAME  = 18/36;
+
+    const MIN_ITERATIONS = PLAYERS*PLAYERS;
+
+    for(let turn = 1; ; turn++)
     {
-        let newStates = new StateCollection();
+        newStates.fill(0);
 
-        states.forEach((state) =>
+        for(let distance = 1; distance <= MIDDLE; distance++)
         {
-            if (state.loser)
+            let last = states[distance];
+
+            newStates[distance]   += SAME * last;
+            newStates[distance-1] += MINUS1 * last;
+
+            if (distance == 1)
             {
-                result += turn * state.probability;
+                newStates[distance]   += MINUS2 * last;
+                newStates[distance+1] += PLUS1 * last;
+                newStates[distance+2] += PLUS2 * last;
+            }
+            else if (distance == MIDDLE-1)
+            {
+                newStates[distance]   += PLUS2 * last;
+                newStates[distance-2] += MINUS2 * last;
+                newStates[distance+1] += PLUS1 * last;
+            }
+            else if (distance == MIDDLE)
+            {
+                newStates[distance-1] += PLUS1 * last;
+                newStates[distance-2] += (MINUS2 + PLUS2) * last;
             }
             else
             {
-                newStates.add(state.createNext(1, 1, 1/36)); // two 1
-                newStates.add(state.createNext(-1,-1,1/36)); // two 6
-                newStates.add(state.createNext(1,-1, 1/36)); // a 1 and a 6
-                newStates.add(state.createNext(-1,1, 1/36)); // a 6 and a 1
-                newStates.add(state.createNext(1, 0, 4/36)); // a 1 and neither 1 or 6
-                newStates.add(state.createNext(-1,0, 4/36)); // a 6 and neither 1 or 6
-                newStates.add(state.createNext(0, 1, 4/36)); // neither 1 or 6 and a 1
-                newStates.add(state.createNext(0,-1, 4/36)); // neither 1 or 6 and a 6
-                newStates.add(state.createNext(0, 0,16/36)); // neither 1 or 6 for both dices
+                newStates[distance-2] += MINUS2 * last;
+                newStates[distance+1] += PLUS1 * last;
+                newStates[distance+2] += PLUS2 * last;
             }
-        });
+        }
 
+        let old = states;
         states = newStates;
+        newStates = old;
+
+        let delta = turn * states[0];
+        result += delta;
+        if (turn > MIN_ITERATIONS && delta < DELTA)
+            break;
     }
 
-    return result;
+    return result.toPrecision(10);
 }
 
 let answer = timeLog.wrap('', () => {
-    return solve();
+    return solve(100);
 });
-
 console.log("Answer is", answer);
