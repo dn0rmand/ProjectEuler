9300faa06fbf4622c219aa950e4540a25d06471d
Dominique NORMAND <dominique@normand.us>
2019-11-21 20:50:32 -0500

    wip

--- problem689.js
+++ problem689.js
@@ -1,314 +1,170 @@
 const assert    = require('assert');
 const timeLog   = require('tools/timeLogger');
-const chart     = require('asciichart');
+const FS        = require('fs');
 
-const bigNumber = require('bignumber.js');
+const MAX_LIMITS = 100;
+const TARGET     = 0.5
 
-const DIGITS = 80;
-
-bigNumber.set({ DECIMAL_PLACES: DIGITS });
-
-function smallNumber(value)
+const limits = (function()
 {
-    let number = {
-        value: value,
+    let total  = (Math.PI*Math.PI)/6;
 
-        toFixed:  function(n) { return this.value.toFixed(n); },
-        toString: function(b) { return this.value.toString(b); },
-        plus:     function(v) { return smallNumber(this.value + v.valueOf()); },
-        dividedBy:function(v) { return smallNumber(this.value / v.valueOf()); },
-        gt:       function(v) { return this.value > v.valueOf(); },
-        valueOf:  function() { return this.value; }
-    };
+    const limits = [];
 
-    return number;
-}
-
-// const FLOAT = bigNumber;
-const FLOAT = bigNumber;
-const DOUBLE= bigNumber;
-
-const ONE = FLOAT(1);
-const ZERO= FLOAT(0);
-
-const CALCULATED = (function() {
-    const values = [];
-
-    for (let i = 1; i < 500; i++)
+    for (let i = 1; i <= MAX_LIMITS; i++)
     {
-        values[i] = ONE.dividedBy(i*i);
+        limits[i] = total;
+        total -= 1/(i*i);
     }
 
-    return values;
+    return limits;
 }());
 
-function *binary(value)
+const preCalculation =  (function()
 {
-    // let v = ONE;
-    // let count = 2;
+    const preCalculation = [];
+    for (let i = 1; i <= MAX_LIMITS; i++)
+        preCalculation[i] = 1 / (i*i);
+    return preCalculation;
+})();
 
-    // while (value.gt(0) && count < DIGITS+2)
-    // {
-    //     v = v.dividedBy(2);
-    //     if (value.gte(v))
-    //     {
-    //         value = value.minus(v);
-    //         // assert.equal(1, +(s[count]));
-    //         yield 1;
-    //     }
-    //     else
-    //     {
-    //         // assert.equal(0, +(s[count]));
-    //         yield 0;
-    //     }
-    //     count++;
-    // }
-
-    let s = DOUBLE(value).toString(2);
-    for(let i = 2; i < s.length; i++)
-        yield +(s[i]);
-}
-
-function f(x, max)
+const probabilities =  (function()
 {
-    if (x == 0)
-        return 0;
+    const probabilities = [];
 
-    let sum = ZERO;
-    let idx = 0;
-    for (let d of binary(x))
+    let v = 1;
+    for (let i = 0; i <= MAX_LIMITS; i++, v /= 2)
+        probabilities[i+1] = v;
+
+    return probabilities;
+})();
+
+const FAILED = [];
+const RESULT = [];
+
+function p(MAX_DEEP, trace)
+{
+    function addPass(i)
     {
-        idx++;
-        if (idx >= 500)
-            break;
-
-        if (d)
-        {
-            sum = sum.plus(CALCULATED[idx]);
-
-            if (max && sum.gt(max))
-                return true;
-        }
+        for (let j = i; j <= MAX_DEEP+1; j++)
+            RESULT[j] += probabilities[i];
     }
 
-    if (max)
+    function addFailed(i)
     {
-        return sum.gt(max);
+        for(let j = i; j <= MAX_DEEP+1; j++)
+            FAILED[j] += probabilities[i];
     }
-    else
-        return sum;
-}
 
-function $p(a)
-{
-    const precision = 9;
-
-    const STEP  = 10**-precision;
-    const COUNT = 10**precision;
-
-    let total  = 0;
-    let last   = 0;
-    let traceCount = 0;
-
-    for(let x = 0.36; x < 1; x += STEP)
+    function inner(value, i)
     {
-        if (x > 0.5)
+        if (value > TARGET)
         {
-            total++;
+            addPass(i);
         }
-        else if (f(x, a))
+        else if (i > MAX_DEEP)
         {
-            if (total == 0)
-                console.log(`\r\nFirst = ${x.valueOf()}`);
-
-            total++;
+            if ((value + limits[i]) <= TARGET)
+                addFailed(MAX_DEEP+1);
+        }
+        else if ((value + limits[i+1]) > TARGET)
+        {
+            inner(value + preCalculation[i], i+1);
+            inner(value, i+1);
+        }
+        else if ((value + limits[i]) > TARGET)
+        {
+            addFailed(i+1);
+            inner(value + preCalculation[i], i+1);
         }
         else
         {
-            last = x;
+            addFailed(i);
         }
-
-        if (traceCount++ == 0)
-            process.stdout.write(`\r${x.toFixed(8)} - ${last.toFixed(8)}  `);
-
-        if (traceCount >= 10000)
-            traceCount = 0;
     }
 
-    let probability = FLOAT(total).dividedBy(COUNT);
-    return probability.toFixed(8);
+    for (let i = 0; i <= MAX_DEEP+1; i++)
+    {
+        RESULT[i] = 0;
+        FAILED[i] = 0;
+    }
+
+    inner(0, 1);
+
+    if (trace)
+    {
+        console.log('DEEPNESS:', MAX_DEEP);
+        console.log('VALID: ',   RESULT[MAX_DEEP]);
+        console.log('FAILED:',   FAILED[MAX_DEEP]);
+    }
+
+    return RESULT[MAX_DEEP+1].toFixed(8);
 }
 
-function p(a, MAX_DEEP, trace)
-{
-    const MAX_SIZE = 1E4;
-    const MAX_LIMITS = 60;
-
-    let RESULT = 0;
-
-    const limits = (function() {
-
-        let total = (Math.PI*Math.PI)/6;
-        let limits = [];
-
-        for (let i = 1; i <= MAX_LIMITS; i++)
-        {
-            limits[i] = total;
-            total -= 1/(i*i);
-        }
-
-        return limits;
-    }());
-
-    const preCalculation =  (function()
-    {
-        const preCalculation = [];
-        for (let i = 1; i <= MAX_LIMITS; i++)
-            preCalculation[i] = 1 / (i*i);
-        return preCalculation;
-    })();
-
-    const probabilities =  (function()
-    {
-        const probabilities = [];
-        for (let i = 0; i <= MAX_LIMITS; i++)
-            probabilities[i+1] = 1 / (2**i);
-        return probabilities;
-    })();
-
-    function possible(value, i)
-    {
-        value += limits[i];
-        return (value > a);
-    }
-
-    let traceCount = 0;
-
-    function add(i)
-    {
-        if (i > MAX_LIMITS)
-            return;
-
-        RESULT += probabilities[i];
-        if (trace)
-        {
-            if (traceCount == 0)
-                process.stdout.write(`\r${RESULT.toFixed(8)}  `);
-            if (++traceCount > 200000)
-                traceCount = 0;
-        }
-    }
-
-    function inner1(value, i)
-    {
-        if (value > a)
-        {
-            add(i);
-            return;
-        }
-
-        if (i > MAX_DEEP)
-        {
-            if (possible(value, i))
-            {
-                while (i <= MAX_LIMITS && value <= a)
-                {
-                    value += preCalculation[i++];
-                }
-                add(i-1);
-            }
-            return;
-        }
-
-        if  (possible(value, i+1))
-        {
-            inner1(value + preCalculation[i], i+1);
-            inner1(value, i+1);
-        }
-        else if (possible(value, i))
-        {
-            inner1(value + preCalculation[i], i+1);
-        }
-    }
-
-    function inner2(states, I)
-    {
-        for(let i = I; i <= MAX_DEEP; i++)
-        {
-            let newStates = [];
-            let last = i == MAX_DEEP;
-
-            for (let state of states)
-            {
-                if (possible(state, i+1))
-                {
-                    // Adding a 1
-                    let n = state + preCalculation[i];
-                    if (n > a)
-                    {
-                        add(i+1);
-                    }
-                    else if (! last)
-                    {
-                        newStates.push(n);
-                    }
-                    // Adding a 0
-                    if (! last)
-                        newStates.push(state);
-
-                    // Check if exceeding ARRAY size
-                    if (newStates.length >= MAX_SIZE)
-                    {
-                        inner2(newStates, i+1);
-                        newStates = [];
-                    }
-                }
-                else if (possible(state, i))
-                {
-                    // Adding a 1
-                    let n = state + preCalculation[i];
-                    if (n > a)
-                    {
-                        add(i+1);
-                    }
-                    else if (! last)
-                    {
-                        newStates.push(n);
-                        if (newStates.length >= MAX_SIZE)
-                        {
-                            inner2(newStates, i+1);
-                            newStates = [];
-                        }
-                    }
-                }
-            }
-
-            states = newStates;
-        }
-    }
-
-    RESULT = 0;
-    inner1(0, 1);
-    return RESULT.toFixed(8);
-}
-
-//assert.equal(p(0.5, 30), 0.54888289);
-
+assert.equal(p(30), 0.54888289);
 console.log('Test passed');
 
-let answer = timeLog.wrap('', () => $p(0.5, 33, true));
+const path1 = [];
+const path2 = [];
+const path3 = [];
+const labels= [];
 
-console.log("Answer is", answer);
+const SCALE = 100;
+const MAX   = 45;
 
-// 0.55594441
-// 0.55627444
-// 0.55619300
-// 0.55619254
-// 0.55601940
-// 0.55620195
-// 0.55620026
-// 0.55619609
-// 0.55619785
-// 0.55710785
-// 0.55923956
-// 0.55923519
+timeLog.wrap('', () => p(MAX, true));
+
+for (let i = 20; i <= MAX+1; i++)
+{
+    labels.push(i);
+    path1.push(SCALE * RESULT[i]);
+    path2.push(SCALE * (1-FAILED[i]));
+    path3.push((SCALE * RESULT[i]) / (RESULT[i] + FAILED[i]));
+}
+
+const html = `
+<html>
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+    </head>
+    <body>
+        <canvas id="myChart" width="auto" height="auto"></canvas>
+
+        <script>
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [${labels.join(', ')}],
+                    datasets: [{
+                        label: 'Pass',
+                        fill: false,
+                        borderColor: "rgb(255, 99, 132)",
+                        data: [ ${ path1.join(', ')} ]
+                    },
+                    {
+                        label: '1-Fail',
+                        fill: false,
+                        borderColor: "rgb(54, 162, 235)",
+                        data: [ ${ path2.join(', ')} ]
+                    },
+                    {
+                        label: 'Pass/(Pass+Fail)',
+                        fill: false,
+                        borderColor: "rgb(54, 235, 162)",
+                        data: [ ${ path3.join(', ')} ]
+                    }
+                ]
+                },
+            });
+        </script>
+    </body>
+</html>
+`;
+
+FS.writeFileSync("chart.html", html);
+
+console.log('\rChart generated');
+// let answer = timeLog.wrap('', () => p(35, true));
+
+// console.log("Answer is", answer);
