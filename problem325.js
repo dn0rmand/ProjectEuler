6c8a7ea2f8a7c74071db0cfbb70ad0ab7d7c9e80
Dominique NORMAND <dominique@normand.us>
2020-01-27 08:59:05 -0500

    WIP

--- problem325.js
+++ problem325.js
@@ -1,14 +1,13 @@
 const assert = require('assert');
-const bigNumber = require('bignumber.js');
 const prettyTime= require("pretty-hrtime");
 
 require('tools/numberHelper');
 require('tools/bigintHelper');
 
-bigNumber.set({ DECIMAL_PLACES: 20 });
-
 const MODULO = 7n ** 10n;
 const MAX    = 10n ** 9n;
+
+const GOLDEN_RATIO = (3+Math.sqrt(5))/2;
 
 function bruteForce(N)
 {
@@ -67,63 +66,50 @@ function bruteForce(N)
             return '' + v;
     }
 
-    let graph = [];
+    let graph = new Array(N+1);
     let total = 0;
 
-    for (let x = 1; x < N; x++)
-    for (let y = x+1; y <= N; y++)
-    {
-        if (isLosing(x, y))
-        {
-            if (graph[x])
-                graph[x].push(F(x+y));
-            else
-                graph[x] = [F(x+y)];
+    graph[0] = new Array(N+1);
+    graph[0].fill(' ');
 
-            total += (x+y);
+    for (let x = 1; x <= N; x++)
+    {
+        graph[x] = new Array(N+1);
+        graph[x].fill(' ');
+
+        for (let y = x+1; y <= N; y++)
+        {
+            if (isLosing(x, y))
+            {
+                graph[x][y] = '#';
+                total += (x+y);
+            }
+            else
+            {
+                graph[x][y] = '.';
+            }
         }
     }
-    for (let i = 1; i < graph.length; i++)
+    let dots = 0;
+    for (let i = graph.length-1; i > 2 ; i--)
     {
         var a = graph[i];
         if (a)
         {
-            console.log(...a);
+            let d = a.reduce((a, v) => a += (v == '.' ? 1 : 0), 0);
+            console.log(a.join(''), ':', F(i));
+            dots += d;
         }
-        else
-            console.log(`no entry for x = ${i}`);
     }
 
-    return total;
+    return {total, dots};
 }
 
-function growing2(callback)
+function S0(N, trace)
 {
-    let l1 = 1, o1 = 1;
-    let l2 = 2, o2 = 1;
+    // sum x = 3 .. N-1 ( sum y = x+1 .. N ( x+y) )
+    const EXPECTED_STEPS = Math.ceil(Number(N) / GOLDEN_RATIO)+1;
 
-    if (callback(l1, o1) === false)
-        return;
-    if (callback(l2, o2) === false)
-        return;
-
-    while(true)
-    {
-        l = l1+l2;
-        o = o1+o2;
-
-        if (callback(l, o) === false)
-            break;
-
-        l1 = l2;
-        o1 = o2;
-        l2 = l;
-        o2 = o;
-    }
-}
-
-function S2(N, trace)
-{
     function evenSum(N)
     {
         let n  = N / 2n;
@@ -135,42 +121,53 @@ function S2(N, trace)
         return (N*(N+1n) / 2n) - evenSum(N);
     }
 
-    function *growing1(callback)
+    function *growing()
     {
-        const FLOOR   = 3;
-        const tau     = bigNumber(5).sqrt().minus(1).dividedBy(2);
-        let   current = tau;
-    
-        while (1)
-        {
-            const previous = current;
-            current = previous.plus(tau).minus(previous.integerValue(FLOOR));
-            const x = current.integerValue(FLOOR).toNumber();
-            yield x ? 1 : 0;
-        }
-    }
-
-    function *growing2()
-    {
-        let first = { value:1 }
+        let previous = { value: 1};
+        let first = { value:1 };
         let last  = first;
+        let count = 1;
+        let remainingSteps = EXPECTED_STEPS;
 
         while (true)
         {
             let l = last;
-            last = { value: 1 }
+
+            last = previous;
+            last.next = undefined;
+            previous = first;
 
             yield first.value;
-            if (first.value == 1)
-                l.next = { value:0, next: last }
-            else
-                l.next = last;
+            remainingSteps--;
 
+            if (first.value == 1)
+            {
+                count++;
+                l.next = {value: 0, next: last};
+            }
+            else
+            {
+                l.next = last;
+                previous.value = 1;
+            }
+            first = first.next;
+
+            if (count >= remainingSteps)
+            {
+                if (trace)
+                    process.stdout.write('\rLoaded enough fibonacci words\n');
+                break;
+            }
+        }
+
+        while (first !== undefined)
+        {
+            yield first.value;
             first = first.next;
         }
-    }
 
-    growing = growing2;
+        throw "Ran out of value!!!!";
+    }
 
     N = BigInt(N);
 
@@ -185,11 +182,9 @@ function S2(N, trace)
         {
             if (traceCount++ == 0)
                 process.stdout.write(`  \r${bottom - top}`);
-            if (traceCount >= 5000)
+            if (traceCount >= 500000)
                 traceCount = 0;
         }
-
-        let sum = 0n;
 
         if (top & 1n) // odd
         {
@@ -210,52 +205,72 @@ function S2(N, trace)
             top += 5n;
         else
             top += 3n;
+
     }
 
     if (trace)
-        console.log('\r       \r');
+        process.stdout.write('\r       \r');
 
     return Number(total);
 }
 
-function S1(N, trace)
+function S2(N, trace)
 {
-    N = Number(N);
+    let F0 = { area: 2n, width: 2n, height: 1n };
+    let F1 = { area: 4n, width: 3n, height: 2n };
 
-    const tau    = (3 - Math.sqrt(5)) / 2;
-    const modulo = Number(MODULO);
+    let s2    = F0.area + F1.area + (F1.width* F0.height);
+    let y     = 6n;
+    let x     = 10n;
 
-    let total       = 0;
-    let traceCount  = 0;
-
-    for (let i = 3; i <= N; i++)
+    while (x < N)
     {
-        if (trace === true)
-        {
-            if (traceCount++ == 0)
-                process.stdout.write(`  \r${N - i}`);
-            if (traceCount >= 1000)
-                traceCount = 0;
-        }
+        let F = {
+            width:  F0.width  + F1.width,
+            height: F0.height + F1.height,
+            area:  (F0.width  * F1.height) + F0.area + F1.area
+        };
 
-        const count = Math.floor(i * tau) % modulo;
-        const v2    = (count.modMul(count, modulo) + count) % modulo;
-        const v1    = i.modMul(count, modulo).modMul(4, modulo);
+        F0 = F1;
+        F1 = F;
 
-        total = (total + v1 - v2) % modulo;
+        let s = (F.width * (y-3n));
+        s2 += s + F.area;
+
+        y += F.height;
+        x += F.width;
     }
 
-    total = total.modDiv(2, modulo);
-
-    if (trace)
-        console.log('');
-
-    return total;
+    return s2;
 }
 
-let S = S2;
+function S(N, trace)
+{
+    N = BigInt(N);
 
-bruteForce(20); // 28389
+    const s1 = ((N**3n - 2n*N**2n - 9n*N + 18n) / 2n) % MODULO;
+    const s2 = S2(N, trace);
+
+    let total;
+
+    if (s1 < s2)
+        total = s1 - s2 + Number(MODULO);
+    else
+        total = s1 - s2;
+
+    return {total, dots: s2};
+}
+
+function analyse(n)
+{
+    const { total: total1, dots: dots1 } = bruteForce(n); // 39983
+    const { total: total2, dots: dots2 } = S(n);
+
+    assert.equal(dots2, dots1);
+}
+
+analyse(35);
+analyse(56);
 
 assert.equal(S(10), 211);
 assert.equal(S(50), 28389);
@@ -263,13 +278,13 @@ assert.equal(S(10000), 230312207313n % MODULO);
 assert.equal(S(1000000), 131546468);
 assert.equal(S(10000000), 38318073);
 
+process.exit(0);
+
 let timer = process.hrtime();
 let answer = S(MAX, true);
 
-// 1E8 => 258929048 28 seconds
-// 1E9 => 163270110 12 minutes 46 seconds
+// 1E8 => 258929048 46 seconds
+// 1E9 => 139748287 7 minutes 4 seconds
 
 timer = process.hrtime(timer);
 console.log('Answer is', answer, "calculated in ", prettyTime(timer, {verbose:true}));
-
-
