23f207e0fbaa13c05c310f96e81c5fd6496fb4ac
Dominique NORMAND <dominique@normand.us>
2020-02-12 21:08:49 -0500

    Removed Debug stuff

--- problem701.js
+++ problem701.js
@@ -1,9 +1,5 @@
 const assert = require('assert');
-// const BigMap = require('tools/BigMap');
-// const BigSet = require('tools/BigSet');
 const timeLogger = require('tools/timeLogger');
-
-const DEBUG = 0
 
 class CompressedState
 {
@@ -20,33 +16,6 @@ class CompressedState
 
         if (state)
             this.init(state);
-    }
-
-    sort()
-    {
-        if (! DEBUG)
-            return;
-
-        if (this.areas.length <= 1)
-            return;
-
-        let m = this.areas.map((value, index) => { return { value, index: index+1} })
-                          .sort((a, b) => a.value - b.value)
-                          .reduce((a, v, index) => { a[v.index] = index+1; return a; }, [0]);
-
-        this.areas.sort((a, b) => a - b);
-
-        if (this.vKey)
-            for(let k = 0; k < this.vKey.length; k++)
-            {
-                this.vKey[k] = m[this.vKey[k]];
-            }
-
-        if (this.hKey)
-            for(let k = 0; k < this.hKey.length; k++)
-            {
-                this.hKey[k] = m[this.hKey[k]];
-            }
     }
 
     init(state) 
@@ -96,27 +65,18 @@ class CompressedState
             }
         }
         state.data = old;
-        
-        if (DEBUG)
-        {
-            this.$state = state;
-        }
-        state.sort;
     }
 
     get key()
     {
-        let extra = '';
-        if (DEBUG)
-            extra = `${this.$state.data}:`
         if (this.vKey && this.hKey)
-            return `${extra}${this.maxArea}:${this.vKey.join('-')}:${this.hKey.join('-')}:${this.areas.join('-')}`;
+            return `${this.maxArea}:${this.vKey.join('-')}:${this.hKey.join('-')}:${this.areas.join('-')}`;
         else if (this.vKey)
-            return `${extra}${this.maxArea}:${this.vKey.join('-')}:${this.areas.join('-')}`;
+            return `${this.maxArea}:${this.vKey.join('-')}:${this.areas.join('-')}`;
         else if (this.hKey)
-            return `${extra}${this.maxArea}:${this.hKey.join('-')}:${this.areas.join('-')}`;
+            return `${this.maxArea}:${this.hKey.join('-')}:${this.areas.join('-')}`;
         else
-            return `${extra}${this.maxArea}:${this.areas.join('-')}`;
+            return `${this.maxArea}:${this.areas.join('-')}`;
     }
 
     get leftKey()
@@ -139,53 +99,6 @@ class CompressedState
         for(let i of this.hKey)
             k = (k*2) + (i === 0 ? 0 : 1);
         return k;
-    }
-
-    static stitchLR(left, right)
-    {
-        const h = left.height;
-        const w = left.width + right.width - 1;
-
-        const S = new State(w, h);
-
-        for(let y = 0; y < h; y++)
-        {
-            for(let x = 0; x < left.width; x++)
-            {
-                S.set(x, y, left.get(left.width-1-x, y));
-            }
-            assert.equal(S.get(left.width-1, y), right.get(0, y));
-            for(let x = 1; x < left.width; x++)
-            {
-                S.set(left.width-1+x, y, right.get(x, y));
-            }
-        }
-
-        return S;
-    }
-
-    static stitchTB(top, bottom)
-    {
-        const h = top.height + bottom.height - 1;
-        const w = top.width;
-
-        const S = new State(w, h);
-
-        for(let x = 0; x < w; x++)
-        {
-            let y = 0;
-            for(; y < top.height; y++)
-            {
-                S.set(x, y, top.get(x, top.height-1-y));
-            }
-            assert.equal(S.get(x, y-1 ), bottom.get(x, 0));
-            for(let i = 1; i < top.height; i++, y++)
-            {
-                S.set(x, y, bottom.get(x, i));
-            }
-        }
-
-        return S;
     }
 
     static mergeLR(left, right)
@@ -291,27 +204,6 @@ class CompressedState
         }
 
         state.maxArea = Math.max(state.maxArea, ... state.areas);
-
-        if (DEBUG)
-        {
-            state.$state = CompressedState.stitchLR(left.$state, right.$state);
-
-            if (state.maxArea != state.$state.getMaxArea() ||
-                (state.areas.length > 1 && state.areas[0] === state.areas[1] && state.hKey.join('') === "00012"))
-            {
-                state.$state.dump();
-                console.log(state.maxArea, state.$state.getMaxArea(), ': ', state.hKey.join(''), ' - ', state.areas.join(','));
-                console.log(left.maxArea, right.maxArea);
-                console.log(left.vKey.join(''), left.areas.join(', '));
-                console.log(right.vKey.join(''), right.areas.join(', '));
-                left.$state.dump();
-                right.$state.dump();
-
-                CompressedState.mergeLR(left, right);
-            }
-        }
-
-        state.sort();
         return state;
     }
     
@@ -390,26 +282,6 @@ class CompressedState
         }
 
         state.maxArea = Math.max(state.maxArea, ... state.areas);
-
-        if (DEBUG)
-        {
-            state.$state = CompressedState.stitchTB(top.$state, bottom.$state);
-
-            if (state.maxArea != state.$state.getMaxArea())
-            {
-                state.$state.dump();
-                console.log(state.maxArea, state.$state.getMaxArea());
-                console.log(top.maxArea, bottom.maxArea);
-                console.log(top.hKey.join(''), top.areas.join(', '));
-                console.log(bottom.hKey.join(''), bottom.areas.join(', '));
-                top.$state.dump();
-                bottom.$state.dump();
-
-                CompressedState.mergeTB(top, bottom);
-            }
-        }
-
-        state.sort();
         return state;
     }    
 }
@@ -650,23 +522,11 @@ function solve(size, trace)
     }
 
     const areas = [];
-    let ignoredAreas = [];
-    let ignoredCount= 0;
 
     pass2( pass1( loadStates((size+1)/2, trace) ), (state) => {
         const area = state.maxArea;
         if (area > MAX_AREA)
-        {
-            if (DEBUG)
-                throw "ERROR";
-            else
-            {
-                ignoredAreas[area-MAX_AREA-1] = (ignoredAreas[area-MAX_AREA-1] || 0) + area;
-                ignoredCount += state.count;
-
-                return; // IGNORE
-            }
-        }
+            throw "ERROR";
         areas[area] = (areas[area] || 0) + state.count;
     });
 
@@ -680,82 +540,7 @@ function solve(size, trace)
     if (count > Number.MAX_SAFE_INTEGER || total > Number.MAX_SAFE_INTEGER)
         throw "ERROR";
 
-    if (size == 7)
-    {
-        if (count != 2**49)
-            console.log(`MISSING = ${ 2**49 - count} `);
-        console.log(`Actual answer = ${ total / count }`);
-        if (ignoredCount > 0)
-        {
-            console.log(`Ignored Count = ${ ignoredCount }`);
-            console.log(`Ignored Areas = ${ ignoredAreas.join(', ') }`);
-        }
-
-        const expected = [
-            1,
-            1280128949,
-            176229287225,
-            2370172541160,
-            9326182280866,
-            19456218353604,
-            29180787054751,
-            36097936854979,
-            39770482398634,
-            40623636302296,
-            39600310721716,
-            37509706588390,
-            34927356524950,
-            32178686371964,
-            29421912139916,
-            26746561753300,
-            24218963812296,
-            21883682102512,
-            19749565785812,
-            17795455788742,
-            15990850820922,
-            14307976499256,
-            12723830222506,
-            11220743506424,
-            9787395461370,
-            8419384731456,
-            7118865766410,
-            5893497890598,
-            4755089510429,
-            3718037391496,
-            2797571528190,
-            2007848049708,
-            1359698792275,
-            857702706964,
-            496867869514,
-            260563213706,
-            122088582016,
-            50569493669,
-            18367605400,
-            5814206412,
-            1595064629,
-            376790971,
-            75969780,
-            12911930,
-            1817335,
-            206449,
-            18212,
-            1172,
-            49,
-            1
-        ];
-        
-        for(let i = 0; i <= MAX_AREA; i++)
-        {
-            let v = areas[i] || 0;
-            if (v != expected[i])
-                console.log(`${i}: ${v} instead of ${expected[i]}`);
-        }
-    }
-
     const answer = (total / count).toFixed(8);
-
-    if (DEBUG)
-        console.log(areas.join(', '));
 
     return +answer;
 }
