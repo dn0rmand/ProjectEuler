b11ed802dc0ba4dac2bfa769de8fb7de3d5c4864
Dominique NORMAND <dominique@normand.us>
2020-04-28 22:22:36 -0400

    502 done - First 100%

--- tools/polynomial.js
+++ tools/polynomial.js
@@ -1,15 +1,32 @@
 const assert = require('assert');
 
-require('tools/numberHelper');
+require('tools/bigIntHelper');
 
-function differences(values, modulo)
+function gcd(v1, v2)
+{
+    if (v1 < 0)
+        v1 = -v1;
+    if (v2 < 0)
+        v2 = -v2;
+    return v1.gcd(v2);
+}
+
+function factorial(value)
+{
+    let f = 1n;
+
+    for(let n = 2n; n <= value; n++)
+        f *= n;
+
+    return f;
+}
+
+function differences(values)
 {
     const result = [];
     for(let i = 1; i < values.length; i++)
     {
         let v = values[i] - values[i-1];
-        while (v < 0 && modulo)
-            v += modulo;
         result.push(v);
     }
     return result;
@@ -24,14 +41,14 @@ function isConstant(values)
     return true;
 }
 
-function reduce(values, modulo)
+function reduce(values)
 {
-    let power = 0;
+    let power = 0n;
     
     while (! isConstant(values))
     {
         power++;
-        values = differences(values, modulo);
+        values = differences(values);
     }
 
     if (values.length < 4)
@@ -40,226 +57,143 @@ function reduce(values, modulo)
     return { power , constant: values[0] };
 }
 
-function gcd(a, b)
+function *solve(values, divisor)
 {
-    a = Math.abs(a);
-    b = Math.abs(b);
+    if (divisor === undefined)
+        divisor = 1n;
 
-    return a.gcd(b);
-}
+    let { power, constant } = reduce(values);
 
-function simplify(equation)
-{
-    let d = 0;
+    let n = constant;
+    let d = factorial(power);
+    let i = gcd(n, d);
+    n /= i;
+    d /= i;
 
-    for(let v of equation)
-    {        
-        if (v === 0)
-            continue;
-        if (d === 0)
-            d = v;
-        else
-            d = gcd(v, d);
-        if (d === 1)
+    if (power === 0n)
+    {
+        if (n === 0n)
             return;
+
+        let dd = d * divisor;
+        let g  = gcd(n, dd);
+        n /= g;
+        dd/= g;
+        yield { power, n, d: dd };
+        return;
     }
 
-    if (d !== 1)
+    for(let x = 1n; x < values.length; x++)
     {
-        for(let i = 0; i < equation.length; i++)
-            equation[i] /= d;
-    }
-}
-
-function ensurePositive(equation, index)
-{
-    if (equation[index] < 0)
-    {
-        for(let p = 0; p < equation.length; p++)
-            equation[p] = -equation[p];
-    }
-}
-
-function substract(eq1, eq2, factor)
-{
-    for(let j = 0; j < eq1.length; j++)
-        eq1[j] -= (eq2[j] * factor);
-}
-
-function cloneEquations(equations)
-{
-    return equations.map(eq => [...eq]);
-}
-
-function solve(equations, power, maxPower, coefficients)
-{
-    equations.forEach((eq) => simplify(eq));
-
-    if (maxPower === power)
-    {
-        let coef = undefined;
-        for(let eq of equations)
-        {            
-            const C = { n: -eq[0], d: eq[power] };
-            if (C.n === 0)
-                C.d == 1;
-            if (C.d < 0)
-            {
-                C.d = -C.d;
-                C.n = -C.n;
-            }
-
-            if (! coef)
-                coef = C;
-
-            assert.equal(C.n, coef.n);
-            assert.equal(C.d, coef.d);
-        }
-        coefficients[power] = coef;
-        return coef;
-    }
-
-    // sort equations to have small A for this power first
-    equations.sort((a, b) => Math.abs(a[power]) - Math.abs(b[power]));
-
-    let ref = equations[0];
-    ensurePositive(ref, power)
-
-    const goodOnes = [];
-
-    for(let i = 1; i < equations.length; i++)
-    {
-        let eq = equations[i];
-        if (eq[power] === 0)
+        let v = x ** power;
+        let N = n * v;
+        if (N === 0n)
             continue;
 
-        ensurePositive(eq, power);
-
-        const factor = Math.floor(eq[power] / ref[power]);
-
-        substract(eq, ref, factor);
-
-        if (eq[power] === 0 && eq[power+1])
-            goodOnes.push(eq);
+        values[x] = (values[x] * d) - N;
     }
 
-    if (power === maxPower)
+    yield *solve(values, d * divisor);
+    yield { power, n, d: d * divisor };
+}
+
+function calculate(x, coefficients, modulo)
+{
+    let divisor = 1n;
+    let value = 0n;
+
+    for(let c of coefficients)
     {
-        if (goodOnes === 0)
+        let v;
+        let d = c.d;
+        if (modulo)
         {
-            throw "Unsolvable";
+            v = c.n.modMul(divisor, modulo).modMul(x.modPow(c.power, modulo), modulo);
+
+            divisor = divisor.modMul(d, modulo);
+            value   = value.modMul(d, modulo);
+            value   = (value + v) % modulo;
+        }
+        else
+        {
+            v = (x ** c.power) * divisor * c.n;
+
+            let g = gcd(v, d);
+            v /= g;
+            d /= g;
+
+            divisor *= d;
+            value   *= d;
+            value   += v;
         }
     }
-    else if (goodOnes.length > 0)
+
+    if (modulo)
     {
-        let coef = solve(goodOnes, power+1, maxPower, coefficients);
-        if (coef)
-        {
-            let eq = equations[0];
-            while (eq.length-1 > power)
-            {
-                let c = coefficients[eq.length-1];
-                let v = eq.pop();
-                for(let i = 0; i < eq.length; i++)
-                    eq[i] = eq[i] * c.d;
-
-                eq[0] = eq[0] - (v*c.n);
-            }
-
-
-            let c = { n: -eq[0], d: eq[power] };
-            if (c.d < 0 && c.n < 0)
-            {
-                c.d = -c.d;
-                c.n = -c.n;
-            }
-            assert.notEqual(c.d, 0);
-            let d = gcd(c.n, c.d);
-            c.n /= d;
-            c.d /= d;
-            coefficients[power] = c;
-            return c;
-        }
+        value = value.modDiv(divisor, modulo);
     }
     else
-        throw "Unsolvable";
+    {
+        let d = gcd(value, divisor);
+        value   /= d;
+        divisor /= d;
+        assert.equal(divisor, 1n);
+    }
+    return value;
 }
 
 module.exports = {
-    findPower : function(values, modulo)
+    findPower : function(values)
     {
-        let { power } = reduce(values, modulo);
+        values  = values.map(v => BigInt(v));
+        let { power } = reduce(values);
         return power;
     },
 
-    solve: function(values, modulo)
+    solve: function(values)
     {
-        let { power } = reduce(values, modulo);
+        values = values.map(v => BigInt(v));
 
-        const coefficients = [{ n: -values[0], d: 1}];
-        const equations    = [];
+        let coefficients = [... solve(values)];
 
-        coefficients.length = power+1;
+        return {
+            coefficients,
+            calculate: (x, modulo) => calculate(x, coefficients, modulo)
+        };
+    },
 
-        const C = values[0]; // remove first row since used to get first coeficient
-        for(let x = 1; x < values.length; x++)
-        {
-            let equation = [C - values[x]];
-
-            for(let p = 1; p <= power; p++)
-                equation.push(x.modPow(p, modulo));
-
-            equations.push(equation);
-        }
-
-        if (solve(cloneEquations(equations), 1, power, coefficients))
-        {
-            for(let i = 0; i < coefficients.length; i++)
+    toString: function(coefficients)
+    {
+        const formula = coefficients.reduce((a, c) => {
+            if (c.n !== 0n)
             {
-                let c = coefficients[i];
-                if (c === undefined)
-                    throw "No solution";
-
-                // if (c.d !== 1)
-                // {
-                //     for(let j = 0; j < coefficients.length; j++)
-                //     {
-                //         if (i === j) continue;
-                //         if (coefficients[j] === undefined) continue;
-                //         coefficients[j].n *= c.d;
-                //         coefficients[j].d *= c.d;
-                //     }
-                // }
-            }
-
-            const formula = coefficients.reduce((a, c, index) => {
-                if (c.n !== 0)
+                let s = 1n;
+                if (c.n > 0)
+                    a.push('+')
+                else
                 {
-                    let s = 1;
-                    if (c.n > 0)
-                        a.push('+')
-                    else
-                    {
-                        s = -1;
-                        a.push('-');
-                    }
-                    let b = '';
-                    if (c.d !== 1)
-                        b = `(${s*c.n}/${c.d})`;
-                    else
-                        b = `${s*c.n}`;
-
-                    if (index === 0)
-                        a.push(`${b}`);
-                    else if (index === 1)
-                        a.push(`${b}*X`);
-                    else
-                        a.push(`${b}*X^${index}`);
+                    s = -1n;
+                    a.push('-');
                 }
-                return a;
-            }, []);
+                let b = '';
+                if (c.d !== 1n)
+                    b = `(${s*c.n}/${c.d})`;
+                else
+                    b = `${s*c.n}`;
 
-            return formula.join(' ');
-        }
+                if (c.power === 0)
+                    a.push(`${b}`);
+                else if (c.power === 1)
+                    a.push(`${b}*X`);
+                else
+                    a.push(`${b}*X^${c.power}`);
+            }
+            return a;
+        }, []);
+
+        if (formula[0] === '+')
+            formula.shift();
+        
+        formula.join(' ');
     },
 };
