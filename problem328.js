9c4d79c3b505ad75364a1d1d40ba697dd6a56be8
Dominique NORMAND <dominique@normand.us>
2020-05-09 15:58:36 -0400

    328 done

--- problem328.js
+++ problem328.js
@@ -1,56 +1,203 @@
 const assert = require('assert');
-const Tracer = require('tools/tracer');
 const timeLogger = require('tools/timeLogger');
 
 const MAX = 200000;
 
 const $memoize_C = [];
+const $costs = [];
+const $treeCost = [];
 
-// A276128
-function C(maxValue)
+function calculateCost(offset)
 {
-    function inner(min, max)
+    const generateEmptyTree = offset =>
+    {
+        let totalEntries = ((offset-1)/2 - 1);
+        
+        const addLayer = bottoms =>
+        {
+            const newBottoms = [];
+            for(let i = 0; totalEntries > 0 && i < bottoms.length; i++)
+            {
+                const node = bottoms[i];
+
+                const left = { value: 0 };
+
+                node.left = left;
+                newBottoms.push(left);
+
+                if (--totalEntries > 0)
+                {
+                    const right = { value: 0 };
+                
+                    node.right = right;
+                    newBottoms.push(right);
+                    totalEntries--;
+                }
+            }
+
+            return newBottoms;
+        }
+
+        let tree    = { value: 0 };
+        let bottoms = [tree];
+
+        while (totalEntries > 0)
+        {
+            bottoms = addLayer(bottoms);
+        }
+
+        return tree;
+    }
+
+    const createTree = offset =>
+    {
+        const tree = generateEmptyTree(offset);
+        let nextValue = 0;
+
+        const getNextValue = _ =>
+        {
+            nextValue += 2;
+            return nextValue;
+        }
+
+        const fill = node =>
+        {
+            if (node.left)
+                fill(node.left);
+            node.value = getNextValue();
+            if (node.right)
+                fill(node.right);
+        }
+
+        fill(tree);
+
+        return tree;
+    }
+
+    const getCost = (node, cost, depth) => 
+    {
+        if (node === undefined)
+            return { cost, depth };
+
+        let left = getCost(node.left, cost + node.value, depth+1);
+        let right= getCost(node.right,cost + node.value, depth+1);
+
+        if (right.depth >= left.depth)
+            return right;
+        else
+            return left;
+    };
+
+    if ($treeCost[offset])
+        return $treeCost[offset];
+
+    const tree = createTree(offset);
+    const cost = getCost(tree, 0, 0);
+
+    $treeCost[offset] = cost;
+
+    return cost;
+}
+
+function fastC(max, offset)
+{
+    const getRightCost = offset =>
+    {
+        const value = max - offset;
+        const { cost, depth } = calculateCost(offset);
+
+        return cost + value*depth;
+    }
+
+    let maxOffset = 2;
+    while (maxOffset-1 <= offset)
+        maxOffset *= 2;
+        
+    let start   = max - offset;
+    let result  = start + Math.max($costs[start-1], getRightCost(offset));
+    let best    = offset;
+
+    offset += 8;
+    let minStart = Math.floor(max / 2);
+
+    while (offset < maxOffset)
+    {
+        start = max - offset;
+        if (start < minStart)
+            break;
+
+        if (start < 1)
+            break;
+
+        const v = start + Math.max($costs[start-1], getRightCost(offset));
+
+        if (v < result)
+        {
+            result = v;
+            best   = offset;
+        }
+        else if (v === result)
+        {
+            best = offset;
+        }
+
+        offset += 8;
+    }
+
+    return { value: result, offset: best }
+}
+
+function C(maxValue, offset)
+{
+    const odds = (maxValue & 1) === 0 ? 1 : 0;
+
+    function inner(min, max, _start, _step, _middle)
     {
         if (min >= max)
-            return { 
-                value:0, 
-                guesses: 0, 
-                guess: 0 
-            };
+            return { value:0, guesses: [0] };
 
         if (min+1 === max)
-            return { 
-                value: min, 
-                guesses: 1, 
-                guess: min
-            };
+            return { value: min, guesses: [min] };
 
         if ($memoize_C[min] === undefined)
             $memoize_C[min] = [];
         if ($memoize_C[min][max] !== undefined)
             return $memoize_C[min][max];
 
-        let result = {
-            value: Number.MAX_SAFE_INTEGER,
-            guesses: 0
-        };
+        let result = { value: Number.MAX_SAFE_INTEGER, guesses: [] };
+        let midPoint = Math.floor((min+max)/2);
 
-        let start = Math.floor((min+max)/2);
-        for(let guess = start; guess <= max; guess++)
+        if (! _middle) 
         {
-            const pass2 = inner(min, guess-1);
-            const pass1 = inner(guess+1, max);
+            _middle = midPoint;
+        }
+        else if(midPoint > _middle)
+        {
+            _middle = Math.max(midPoint, _middle);
+        }
 
-            const pass  = pass1.value < pass2.value ? pass2 : pass1;
+        if (! _step)
+            _step = 2;
 
-            if (pass.value+guess < result.value)
+        if (! _start)
+            _start = (_middle - (_middle & 1)) + odds;
+
+        _middle--;
+
+        for (let guess = _start; guess >= _middle && guess <= max; guess += _step)
+        {
+            const pass2 = inner(min, guess-1,  undefined, undefined, undefined);
+            const pass1 = inner(guess+1, max,  undefined, undefined, undefined);
+
+            const pass  = (pass1.value < pass2.value ? pass2 : pass1);
+
+            if (guess + pass.value < result.value)
             {
-                result = { 
-                    value: pass.value+guess, 
-                    guesses: pass.guesses+1, 
-                    guess: guess,
-                    next: pass
-                };
+                result = { value: pass.value + guess, guesses : [guess] };
+            }
+            else if (guess + pass.value === result.value)
+            {
+                result.guesses.push(guess);
             }
         }
 
@@ -59,30 +206,101 @@ function C(maxValue)
         return result;
     }
 
-    let answer = inner(1, maxValue);
+    let start, step, middle;
 
-    return answer.value;
+    if (maxValue >= 100 && offset)
+    {
+        start = maxValue - offset;
+        step  = -8;
+        middle = 2;
+        while (middle <= offset)
+            middle *= 2;
+        middle = (middle*2)-1;
+        middle = (start - middle);
+    }
+
+    let answer = inner(1, maxValue, start, step, middle);
+
+    offset = answer.guesses.reduce((a, v) => Math.max(maxValue - v, a), 0);
+
+    return { value: answer.value, offset};
 }
 
-function solve(max, trace)
+function solve(max)
 {
-    let total = 0;
-    const tracer = new Tracer(100, trace);
-    for (let i = 1; i <= max; i++)
+    let total       = 0;
+    let lastOffset  = 0;
+
+    for(let i = 1; i <= max; i++)
     {
-        tracer.print(_ => max-i);
-        total += C(i, trace);
+        const f = i < 100 ? C : fastC;
+
+        const { value, offset } = f(i, lastOffset);
+
+        total += value;
+        lastOffset = offset;
+        $costs[i] = value;
     }
-    tracer.clear();
+
     return total;
 }
 
-assert.equal(C(1), 0);
-assert.equal(C(2), 1);
-assert.equal(C(8), 12);
-assert.equal(C(100), 400);
-assert.equal(timeLogger.wrap('', _ => solve(100)), 17575);
+
+assert.costs = function()
+{
+    const knownCost = offset =>
+    {
+        switch (offset)
+        {
+            case  15: return { depth: 3, cost: 34 };
+            case  23: return { depth: 4, cost: 50 };
+            case  31: return { depth: 4, cost: 98 };
+            case  39: return { depth: 5, cost: 74 };
+            case  47: return { depth: 5, cost: 130 };
+            case  55: return { depth: 5, cost: 210 };
+            case  63: return { depth: 5, cost: 258 };
+            case  71: return { depth: 6, cost: 114 };
+            case  79: return { depth: 6, cost: 178 };
+            case  87: return { depth: 6, cost: 266 };
+            case  95: return { depth: 6, cost: 322 };
+            case 103: return { depth: 6, cost: 458 };
+            case 111: return { depth: 6, cost: 514 };
+            case 119: return { depth: 6, cost: 594 };
+            case 127: return { depth: 6, cost: 642 };
+            case 135: return { depth: 7, cost: 186 };
+
+            default: return undefined;
+        }
+    }
+
+    for(let offset = 15; offset <= 135; offset += 8)
+    {
+        const ref = knownCost(offset);
+        if (ref === undefined)
+            throw 'No reference to compare to';
+
+        const value = calculateCost(offset);
+
+        if (value.cost !== ref.cost || value.depth !== ref.depth)
+        {
+            console.log(`Invalid calculation for offset ${offset} - {cost: ${value.cost}, depth: ${value.depth}} instead of {cost: ${ref.cost}, depth: ${ref.depth}}`)
+        }
+    }
+
+    return;
+}
+
+assert.costs();
+assert.equal(C(1).value, 0);
+assert.equal(C(2).value, 1);
+assert.equal(C(8).value, 12);
+assert.equal(C(100).value, 400);
+assert.equal(solve(100), 17575);
+assert.equal(timeLogger.wrap('', _ => solve(900)), 2480183);
+assert.equal(timeLogger.wrap('', _ => solve(2000)), 14210783);
 
 console.log("Tests passed");
 
-console.log(timeLogger.wrap('', _ => solve(2000, true)));
+const answer = timeLogger.wrap('', _ => solve(MAX));
+
+console.log(`Answer is ${answer}`);
