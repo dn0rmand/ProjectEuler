2edbb91434c6d7bdcfa427fc83f660d1ec4b9615
Dominique NORMAND <dominique@normand.us>
2020-07-12 09:35:03 -0400

    512 done

---
+++ Solved/problem512.js
@@ -0,0 +1,55 @@
+const assert = require('assert');
+const timeLogger = require('tools/timeLogger');
+const primeHelper = require('tools/primeHelper')();
+const ProgressBar = require('progress');
+
+const MAX = 500000000;
+const MAX_PRIME = Math.ceil(Math.sqrt(MAX)+1);
+
+primeHelper.initialize(MAX_PRIME);
+
+function g(n, trace)
+{
+    let tick = _ => { };
+
+    if (trace)
+    {
+        bar = new ProgressBar('  Calculating :percent [:bar]', {
+            complete: '=',
+            incomplete: ' ',
+            width: 50,
+            total: n,
+            renderThrottle: 1000,
+        });
+
+        tick = _ => bar.tick();
+    }
+
+    let total = 0;
+    let extra = 0n;
+
+    for(let i = 1; i <= n; i += 2)
+    {
+        tick();
+        tick();
+        const p = primeHelper.PHI(i);
+        const t = total + p;
+        if (t > Number.MAX_SAFE_INTEGER)
+        {
+            extra += BigInt(total);
+            total  = p;
+        }
+        else
+            total = t;
+    }
+    if (trace)
+        console.log();
+
+    return BigInt(total)+extra;
+}
+
+assert.equal(g(100), 2007);
+console.log('Test passed');
+
+const answer = timeLogger.wrap('', _ => g(MAX, true));
+console.log(`Answer is ${answer}`);
