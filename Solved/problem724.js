2f70fd3ad6f566f4b7c6180be454ad25a3136d0c
Dominique NORMAND <dominique@normand.us>
2020-09-06 15:02:57 -0400

    724 done

---
+++ Solved/problem724.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const timeLogger = require('tools/timeLogger');
+
+// http://oeis.org/A027459
+
+function solve(n)
+{
+    let hk = 0;
+    let total = 0;
+
+    for(let k = 1; k <= n; k++)
+    {
+        hk += n/k;
+
+        total += hk / k;
+    }
+
+    return total;
+}
+
+assert.equal(solve(2), 7 / 2);
+assert.equal(solve(5).toFixed(6), (12019 / 720).toFixed(6));
+assert.equal(solve(100).toFixed(7), 1427.1934705);
+
+const answer = timeLogger.wrap('', _ => solve(1E8));
+
+console.log(`Answer is ${Math.round(answer)}`);
