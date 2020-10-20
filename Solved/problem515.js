c51cee5e576250bf4d8bba7b96ed35078001aa0f
Dominique Normand <dnormand@us.imshealth.com>
2020-10-20 09:23:18 -0400

    515 done

---
+++ Solved/problem515.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const primeHelper = require('tools/primeHelper')();
+const timeLogger = require('tools/timeLogger');
+const Tracer = require('tools/tracer');
+
+require('tools/numberHelper');
+
+const MAX_A = 1E9;
+const MAX_B = 1E5;
+const MAX_K = 1E5;
+
+const MAX_PRIME = MAX_A + MAX_B;
+
+const primes = timeLogger.wrap('Loading Primes', _ => {
+    primeHelper.initialize(MAX_PRIME, true);
+
+    return primeHelper.allPrimes();
+});
+
+function D(a, b, k)
+{
+    let total = 0;
+
+    for(const p of primes)
+    {
+        if (p < a)
+            continue;
+        if (p >= a+b)
+            break;
+
+        total += (k-1).modInv(p);
+    }
+    return total;
+}
+
+assert.strictEqual(D(101, 1, 10), 45);
+assert.strictEqual(D(1E3, 1E2, 1E2), 8334);
+assert.strictEqual(timeLogger.wrap('1E6', _ => D(1E6, 1E3, 1E3)), 38162302);
+
+console.log('Tests passed');
+
+const answer = timeLogger.wrap('', _ => D(MAX_A, MAX_B, MAX_K, true)) ;
+
+console.log(`Answer is ${answer}`);
