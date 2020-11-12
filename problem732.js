94355741f73a6278ad14dcc746dfc39573c855ab
Dominique NORMAND <dominique@normand.us>
2020-11-12 08:11:48 -0500

    372 done ... brute force finds the value right away but doesn't finish ( too long ) The smart one doesn't work :(

--- problem732.js
+++ problem732.js
@@ -2,21 +2,20 @@ const assert = require('assert');
 const Tracer = require('tools/tracer');
 const timeLogger = require('tools/timeLogger');
 
+const dummyGC = () => {};
+
+const GC = global.gc || dummyGC;
+
 require('tools/numberHelper');
 
 const FIVE = 5;
 const MODULO_1 = 1000000007;
 const MODULO_2 = 101;
 
-function R(n)
+function createTrolls(n, useIds)
 {
-    const r = (FIVE.modPow(n, MODULO_1) % MODULO_2) + 50;
-    
-    return  r;
-}
+    const R = (n) => (FIVE.modPow(n, MODULO_1) % MODULO_2) + 50;
 
-function createTrolls(n)
-{
     const trolls = [];
 
     for(let i = 0; i < n; i++)
@@ -25,68 +24,97 @@ function createTrolls(n)
         const l = R(3*i + 1);
         const q = R(3*i + 2);
 
-        trolls.push({ h, l, uniqueId: i, q });
+        const t = { escape: false, h, l, q, uniqueId: i };
+
+        trolls.push(t);
     }
 
     trolls.sort((a, b) => {
-        if (a.h !== b.h)
-            return b.h - a.h;
+        let x1 = a.q / Math.pow(a.h, 1.01);
+        let x2 = b.q / Math.pow(b.h, 1.01);
+        if (x1 !== x2)
+            return x1-x2;
+
+        x1 = a.q / a.l;
+        x2 = b.q / b.l;
+        if (x1 !== x2)
+            return x1-x2;
+
         if (a.q !== b.q)
             return a.q - b.q;
+        if (a.h !== b.h)
+            return b.h - a.h;
         if (a.l !== b.l)
             return b.l - a.l;
         return 0;
     })
 
-    let idx = 0;
-    let previous = undefined;
-    let same = troll => {
-        if (! previous)
-            return false;
+    if (useIds) {
+        let idx = 0;
+        let previous = undefined;
+        let same = troll => {
+            if (! previous)
+                return false;
 
-        return troll.h === previous.h && troll.l === previous.l && troll.q === previous.q;
+            return troll.h === previous.h && troll.l === previous.l && troll.q === previous.q;
+        }
+
+        trolls.forEach(troll => {
+            if (same(troll))
+                troll.id = previous.id;
+            else
+                troll.id = ++idx;
+            previous = troll; 
+        });
     }
-
-    trolls.forEach(troll => {
-        if (same(troll))
-            troll.id = previous.id;
-        else
-            troll.id = ++idx;
-        previous = troll; 
-    });
 
     return trolls;
 }
 
-
-function Q(n, trace)
+function getValue(trolls, height)
 {
-    const memoize = {};
+    let value = 0;
+    
+    for(const t of trolls)
+    {
+        height -= t.h;
+        if (t.l >= height)
+        {
+            t.escape = true;
+            value += t.q;
+        }
+        else
+        {
+            t.escape = false;
+        }
+    }
+    return value;
+}
+
+function bruteQ(n, trace)
+{
+    const memoize = new Map();
     const tracer = new Tracer(1, trace);
 
-    const allTrolls = createTrolls(n);
+    const allTrolls = createTrolls(n, true);
     const D         = Math.ceil(allTrolls.reduce((a, t) => a + t.h, 0) / Math.sqrt(2));
 
-    let maximum = 0;
+    console.log(getValue(allTrolls, D));
+
     let biggest = 0;
 
-    function getMaxQ(trolls, height)
+    function add(key, max)
     {
-        let q = 0;
-
-        trolls.forEach(t => t.unused = true);
-
-        for(const troll of allTrolls) 
-        {
-            if (!troll.unused && troll.l >= height && troll.q > q)
-            {
-                q = troll.q;
-            }
+        if (memoize.size > 3000000) {
+            memoize.clear();
+            GC();
         }
 
-        trolls.forEach(t => t.unused = false);
-
-        return q;
+        memoize.set(key, max);
+        if (max > biggest) {
+            biggest = max;
+            tracer.print(_ => biggest);
+        }
     }
 
     function inner(height, trolls)
@@ -94,22 +122,18 @@ function Q(n, trace)
         if (height < 0)
             height = 0;
 
-        const key = `${height}-${trolls.map(t => t.id).join(':')}`;
-        if (memoize[key])
-            return memoize[key];
+        const key = `${height}:${trolls.map(t => t.id).sort((a, b) => a-b).join(',')}`;
+        let max = memoize.get(key);
+        if (max)
+            return max;
 
-        if (height <= 0) {
-            const max = trolls.reduce((max, troll) => max + troll.q, 0);
-            memoize[key] = max;
-            if (max > biggest)
-            {
-                biggest = max;
-                tracer.print(_ => max);
-            }
+        if (height <= 100) { // every h+l is at least 100
+            max = trolls.reduce((max, troll) => max + troll.q, 0);
+            add(key, max);
             return max;
         }
         else {
-            let max = 0;
+            max = 0;
             let maxPossible = trolls.reduce((a, t) => a + t.q, 0);
             let previousID = -1;
 
@@ -121,7 +145,6 @@ function Q(n, trace)
                 const newTrolls = trolls.slice(0, i).concat(trolls.slice(i+1));
 
                 let m = inner(height-troll.h, newTrolls);
-                let m0 = m;
 
                 if ((troll.h+troll.l) >= height)
                     m += troll.q;
@@ -131,23 +154,67 @@ function Q(n, trace)
                 if (max > biggest)
                 {
                     biggest = max;
-                    const couldHaveBeen = m0 + getMaxQ(newTrolls, height-troll.h);                    
-                    tracer.print(_ => `${max} - ${couldHaveBeen}`);
+                    tracer.print(_ => biggest);
                 }
 
                 if (max >= maxPossible)
                     break;
             }
 
-            memoize[key] = max;
+            add(key, max);
             return max;
         }
     }
 
-    maximum = inner(D, allTrolls);
+    const maximum = inner(D, allTrolls);
+    
+    tracer.clear();
 
     return maximum;
 }
+
+function smartQ(n, trace)
+{
+    const tracer = new Tracer(1, trace);
+
+    const trolls = createTrolls(n, true);
+    const D      = Math.ceil(trolls.reduce((a, t) => a + t.h, 0) / Math.sqrt(2));
+
+    let max = getValue(trolls, D);
+
+    for(let i = 0; i < trolls.length; i++)
+    {
+        const t1 = trolls[i];
+
+        for(let j = trolls.length-1; j > i; j--)
+        {
+            const t2 = trolls[j];
+
+            if (t1.escape && t2.escape)
+                continue;
+
+            trolls[i] = t2;
+            trolls[j] = t1;
+            const m = getValue(trolls, D);
+            if (m > max) {
+                i = -1;
+                max = m;
+                tracer.print(_ => max);
+                break;
+            } else {
+                trolls[i] = t1;
+                trolls[j] = t2;
+                getValue(trolls, D);
+            }
+        }
+    }
+
+    tracer.clear();
+
+    return max;
+}
+
+const Q = bruteQ;
 
 assert.strictEqual(Q(5), 401);
 assert.strictEqual(timeLogger.wrap('Q(15)', _ => Q(15)), 941);
