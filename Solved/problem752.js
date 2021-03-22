c0bbc2fc5ec5ee5d8dfe632aeb9ec22eda9d065a
Dominique Normand <dnormand@us.imshealth.com>
2021-03-21 20:43:06 -0400

    752 done

---
+++ Solved/problem752.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const timeLogger = require('tools/timeLogger');
+const Tracer = require('tools/tracer');
+
+const MAX = 10**6;
+
+function g(x)
+{
+    if ((x % 2) === 0 || (x % 3) === 0) {
+        // even => Nope
+        return 0n;
+    }
+
+    let a = 1;
+    let b = 1;
+
+    const max = x*x;
+
+    for(let n = 2; n < max ; n++) {
+        [a, b] = [(a + b*7) % x, (a+b) % x];
+
+        if (b === 0) {
+            for(const frequency = n, A = a; n < max; n += frequency) {
+                if (a === 1) {
+                    return BigInt(n);    
+                }
+
+                a = (a * A) % x;
+            }
+        }
+    }
+
+    return 0n;
+}
+
+function G(n, trace)
+{
+    let total = 0n;
+
+    const tracer = new Tracer(1000, trace);
+    for (let x = 3; x <= n; x += 2) {
+        tracer.print(_ => n-x);
+        total += g(x, trace);
+    }
+    tracer.clear();
+    return total;
+}
+
+assert.strictEqual(g(3), 0n);
+assert.strictEqual(g(5), 12n);
+
+assert.strictEqual(G(100), 28891n);
+assert.strictEqual(timeLogger.wrap('', _ => G(1000)), 13131583n);
+
+console.log('Tests passed');
+
+const answer = timeLogger.wrap('', _ => G(MAX, true));
+console.log(`Answer is ${answer}`);
