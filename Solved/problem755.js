89ebc46e930a792c75aaa1fc1d2ecd3f0a9a5a14
Dominique Normand <dnormand@us.imshealth.com>
2021-04-18 20:31:19 -0400

    755 done

---
+++ Solved/problem755.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const timeLogger = require('tools/timeLogger');
+
+const MAX = 1E13;
+
+const { fibonacci, sums } = timeLogger.wrap('Loading fibonacci', _ => {
+    const fibonacci = [1];
+    const sums      = [1];
+
+    let f1 = 1;
+    let f2 = 2;
+    let s  = 1;
+
+    while (f2 <= MAX) {
+        s += f2;
+        sums.push(s);
+        fibonacci.push(f2);
+        [f1, f2] = [f2, f1+f2];
+    }
+
+    return { fibonacci, sums };
+});
+
+function solve(n) 
+{
+    function inner(value, index) 
+    {
+        if (value === 0) {
+            return 1n;
+        }
+
+        let total = 1n;
+
+        for(let i = index; i >= 0; i--) {
+            const f = fibonacci[i];            
+            if (f > value) {
+                continue;
+            }
+
+            if (sums[i] <= value) {
+                total += BigInt(2 ** i);
+            } else {
+                total += inner(value-f, i-1);
+            }
+        }
+
+        return total;
+    }
+
+    const total = inner(n, fibonacci.length-1);
+    return total;
+}
+
+assert.strictEqual(solve(100), 415n);
+assert.strictEqual(solve(1E4), 312807n);
+console.log('Tests passed');
+
+const answer = timeLogger.wrap('', _ => solve(MAX));
+console.log(`Answer is ${answer}`);
