e1c6a78c5ddb1bca714b90bfc5346f0764fb8b71
Dominique Normand <dnormand@us.imshealth.com>
2021-08-25 09:15:02 -0400

    290 done

---
+++ Solved/problem290.js
@@ -0,0 +1,96 @@
+const timeLogger = require('tools/timeLogger');
+
+function digitSum(n)
+{
+    let sum = 0;
+
+    while (n > 0) {
+        const d = n % 10;
+        n = (n-d)/10;
+        sum += d;
+    }
+
+    return sum;
+}
+
+function brute(n)
+{
+    const max = 10 ** n;
+
+    let total = 0n;
+
+    for(let value = 0; value <= max; value += 9)
+    {
+        if (digitSum(value) === digitSum(value * 137)) {
+            total++;
+        }
+    }    
+
+    console.log(total);
+    return total;
+}
+
+function solve(n)
+{
+    const $memoize = [];
+
+    function get(prefix, sum, sum137, length)
+    {
+        const diff = sum - sum137;
+
+        let sub = $memoize[prefix];
+        if (! sub) return undefined;
+        let sub2 = sub.get(length);
+        if (! sub2) return undefined;
+        return sub2.get(diff);
+    }
+
+    function set(prefix, sum, sum137, length, value)
+    {
+        const diff = sum - sum137;
+
+        let sub = $memoize[prefix];
+        if (! sub) {
+            sub = new Map();
+            $memoize[prefix] = sub;
+        }
+        let sub2 = sub.get(length);
+        if (! sub2) {
+            sub2 = new Map();
+            sub.set(length, sub2);
+        }
+        sub2.set(diff, value);
+    }
+
+    function inner(prefix, sum, sum137, length) 
+    {
+        if (length === 0) {
+            sum137 += digitSum(prefix);
+            return (sum === sum137 ? 1n : 0n);
+        }
+
+        let count = get(prefix, sum, sum137, length);
+        if (count !== undefined) {
+            return count;
+        }
+        count = 0n;
+        for(let digit = 0; digit < 10; digit++) {
+            const s137 = prefix + digit*137;
+            const d137 = s137 % 10;
+            const p137 = (s137-d137)/10;
+            
+            count += inner(p137, sum + digit, sum137 + d137, length-1);
+        }
+
+        set(prefix, sum, sum137, length, count);
+        return count;
+    }
+
+    const total = inner(0, 0, 0, n);
+
+    return total;
+}
+
+const answer = timeLogger.wrap('', _ => solve(18));
+
+console.log(`Answer is ${answer}`);
--- problem391.js
+++ problem391.js
@@ -26,108 +26,176 @@
 // Find Σ(M(n))3 for 1 ≤ n ≤ 1000.
 
 const assert = require('assert');
+const Tracer = require('tools/tracer');
+const timeLogger = require('tools/timeLogger');
+const BigMap = require('tools/BigMap');
+const BigSet = require('tools/BigSet');
 
 const MAX   = 1000;
 const MAX_S = 1E6;
-const S     = new Set();
 
-function buildS(max)
+const $S = new BigMap();
+const $SS = new BigSet();
+
+let $maxS  = 0;
+let $maxN = 0;
+
+function S(n)
 {
-    let last = 0;
-    let n    = 0;
-
-    while (last <= max)
-    {        
-        let x = n++;
-        while (x > 0)
-        {
-            let d = (x % 2);
-            x = (x-d) / 2;
-            if (d !== 0)
-                last++;
-        }
-        S.add(last);
+    if (n < 1) {
+        return 0;
     }
-}
 
-buildS(MAX_S); 
+    let s = $S.get(n)
+    if (s !== undefined) {
+        return s;
+    }
+
+    if (n & 1) {
+        const m = (n-1)/2;
+        s = 2*S(m) + m + 1;
+    } else {
+        const m = n / 2;
+        s = S(m) + S(m-1) + m;
+    }
+
+    if (n > $maxN) {
+        if (s > Number.MAX_SAFE_INTEGER) {
+            throw "TOO BIG";
+        }
+        $maxS = s;
+        $maxN = n;
+    }
+
+    $S.set(n, s);
+    $SS.add(s);
+
+    return s;
+}
 
 function isValid(c)
 {
-    if (c > MAX_S)
-        throw "Not enough values in the sequence. " + c + " not defined";
-
-    return S.has(c);
-}
-
-function M(n, reverse, start)
-{
-    let memoize = new Map();
-    let player1 = false;
-
-    function winningPosition(c)
-    {
-        let key = player1 ? -c : c;
-
-        let v = memoize.get(key);
-        if (v !== undefined)
-            return v;
-
-        for (let m = n; m > 0; m--)
-        {
-            if (! isValid(c+m))
-                continue;
-            
-            player1 = !player1;
-            let win = winningPosition(c+m);
-            player1 = !player1;
-
-            if (player1 && win)
-            {
-                memoize.set(key, true);
+    if (c > $maxS) {
+        while ($maxS < c) {
+            const s = S($maxN+1);
+            if (s === c) {
                 return true;
             }
-            else if (! player1 && ! win)
-            {
-                memoize.set(key, false);
-                return false;
-            }
-        }    
-
-        memoize.set(key, ! player1);
-        return ! player1;    
-    }
-
-    if (start === undefined)
-        start = 0;
-
-    let result = 0;
-
-    for (let c = n; c > 0; c--)
-    {
-        if (! isValid(c + start))
-            continue;
-
-        if (reverse)
-        {
-            let x = M(n, false, c+start);
-            if (x !== 0)
-            {
-                result = c;
-                break;
-            }
         }
-        else
+        return false;
+    } else {
+        return $SS.has(c);
+    }
+}
+
+function M(max, trace)
+{
+    class State {
+        constructor(position, player)
         {
-            player1 = false;
-            if (winningPosition(c+start))
-            {
-                result = c;
-                break;
+            this.position = position;
+            this.player   = player;
+            this.key      = position * player; 
+        }
+
+        *moves(direction = -1) {
+            for(let i = 1; i <= max; i++) {
+                const p = this.position + i*direction;
+                if (isValid(p)) {
+                    yield new State(p, -this.player); 
+                }
             }
         }
     }
-    return result;
+
+    // let idx = 1;
+
+    // if (trace) {
+    //     timeLogger.wrap('calculating start position', _ => {
+    //         const t = new Tracer(1000, trace);
+    //         let maxDiff = 0;
+    //         // Find winning target
+    //         while (S(idx+1)-S(idx) <= max) {
+    //             maxDiff = Math.max(maxDiff, S(idx+1)-S(idx));
+    //             idx++;
+    //             t.print(_ => `${idx} - ${maxDiff}`);
+    //         }
+    //         t.clear();
+    //     });
+    // } else {
+    //     while (S(idx+1)-S(idx) <= max) {
+    //         idx++;
+    //     }
+    // }
+
+    // const P1 = S(idx);
+    const p = (2**max) * (max+1) - (max+1);
+
+    let visited = new Set();
+    let minVisited = p+1;
+
+    let states = new Map();
+    let newStates = new Map();
+    let best = 0;
+
+    const s = new State(p, -1);
+
+    states.set(s.key, s);
+
+    const tracer = new Tracer(10000, trace);
+    while(states.size > 0) {
+        tracer.print(_ => minVisited);
+
+        newStates.clear();
+
+        states.forEach(state => {
+            if (state.player === 1) {
+                visited.add(state.key);
+                if (state.key < minVisited) {
+                    minVisited = state.key;
+                }
+            }
+        });
+
+        if (visited.size > 1E4) {
+            // Filter visited to last max values
+            const maxVisited = minVisited + max + 1;
+
+            let newVisited = new Set();
+            visited.forEach(v => {
+                if (v <= maxVisited) {
+                    newVisited.add(v);
+                }
+            });
+
+            visited = newVisited;
+        }
+
+        for(const state of states.values()) {
+            if (state.player === -1 && state.position <= max && state.position > best) {
+                best = state.position;
+            }
+
+            for(const newState of state.moves(-1)) {
+                let good = true;
+                if (newState.player === -1) {
+                    for(const forward of newState.moves(1)) {
+                        if (! visited.has(forward.key)) {
+                            good = false;
+                            break;
+                        }
+                    }
+                }
+                if (good) {
+                    newStates.set(newState.key, newState);
+                }
+            }
+        }
+
+        [states, newStates] = [newStates, states];
+    }
+    tracer.clear();
+    return best;
 }
 
 function solve(value)
@@ -143,15 +211,46 @@ function solve(value)
     return total;
 }
 
-assert.equal(M(20), 4);
-assert.equal(M(2), 2);
-assert.equal(M(7), 1);
+const values = [0, 2, 1, 4, 2, 2, 1, 7, 5, 5, 7, 4, 13, 7, 15, 2, 9, 1, 7, 4, 15, 5, 9];
+for(let i = values.length-1; i > 0; i--) {
+    const v = values[i];
+    const b = {};
+    for(let mask = 1; mask <= i; mask *= 2) {
+        if (i & mask) {
+            b[mask] = values[mask];
+        }
+    }
+    console.log(`M(${i})=${v} -> ${JSON.stringify(b)}`);
+}
+console.log(values.join(', '));
 
-assert.equal(solve(10), 683);
-assert.equal(solve(13), 3287);
-assert.equal(solve(20), 8150);
+assert.strictEqual(M(2), 2);
+assert.strictEqual(M(7), 1);
+assert.strictEqual(M(4), 4);
+assert.strictEqual(timeLogger.wrap('M(20)', _ => M(20, true)), 4);
 
-let answer = solve(MAX);
+// console.log(timeLogger.wrap('M(22)', _ => M(22, true)));
+// console.log(timeLogger.wrap('M(22)', _ => M(22, false)));
 
-console.log("Answer is " + answer + " (61029882288)");
-console.log('Done');
+// assert.strictEqual(solve(10), 683);
+// assert.strictEqual(solve(13), 3287);
+// assert.strictEqual(solve(20), 8150);
+
+console.log('Tests passed');
+
+// let answer = solve(MAX);
+
+// console.log("Answer is " + answer + " (61029882288)");
+// console.log('Done');
+
+/*
+0,2,1,4,2,2,1,7,5,5,7,4,13,7,15,2,9,1,7,4
+
+
+0, 1, 2, 4, 5, 7, 9, 12, 13, 15, 17, 20, 22, 25, 28, 32, 33, 35, 37, 40, 42, 45, 48, 52, 54, 57, 60, 64, 67, 71, 75, 80
+
+1
+     L    W    L
+0 -> 1 -> 2 -> 3
+               true 
+*/
