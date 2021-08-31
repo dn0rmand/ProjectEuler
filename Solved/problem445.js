a2007a5ab4a25b9ac6e6b1abc31f636fe435057b
Dominique Normand <dnormand@us.imshealth.com>
2021-08-31 00:51:05 -0400

    445 done

---
+++ Solved/problem445.js
@@ -0,0 +1,83 @@
+const assert = require('assert');
+const Tracer = require('tools/tracer');
+const timeLogger = require('tools/timeLogger');
+const primeHelper = require('tools/primeHelper')();
+const MAX = 1E7;
+const MODULO = 1000000007;
+
+require('tools/numberHelper');
+
+primeHelper.initialize(MAX);
+
+const $modInv = new Uint32Array(MODULO);
+
+function modInv(n) 
+{
+    if (!$modInv[n]) {
+        $modInv[n] = n.modInv(MODULO);
+    }
+    return $modInv[n];
+}
+
+function solve(n)
+{
+    const tracer = new Tracer(50000, true);
+
+    let total = 0;
+    let R = 1;
+    let nCk = 1;
+    let nCkSum = 0;
+
+    const factors = new Uint32Array(n+1);
+    const changed = new Map();
+
+    for(let k = 1; k < n; k++) {
+        tracer.print(_ => n-k);
+        nCk = nCk.modMul(n+1-k, MODULO).modMul(modInv(k), MODULO);
+        nCkSum = (nCkSum + nCk) % MODULO;
+
+        changed.clear();
+
+        primeHelper.factorize((n+1-k), (p, f) => {
+            changed.set(p, f);
+        });
+
+        primeHelper.factorize(k, (p, f) => {
+            changed.set(p, (changed.get(p) || 0) - f);
+        });
+
+        changed.forEach((f, p) => {
+            const $old = factors[p] || 0;
+            const $new = $old + f;
+
+            if ($old === $new) {
+                return;
+            }
+
+            factors[p] = $new;
+
+            if ($new) {                
+                R = R.modMul(p**$new + 1, MODULO);
+            }
+
+            if ($old) {
+                R = R.modMul(modInv(p**$old + 1), MODULO);
+            }
+        });
+
+        total = (total + R) % MODULO;
+    }
+
+    total = (total + MODULO - nCkSum) % MODULO;
+
+    tracer.clear();
+    return total;
+}
+
+assert.strictEqual(solve(20), 1045006);
+assert.strictEqual(timeLogger.wrap('', _ => solve(100000)), 628701600);
+
+console.log('Tests passed');
+
+const answer = timeLogger.wrap('', _ => solve(MAX));
+console.log(`Answer is ${answer}`);
