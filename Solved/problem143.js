abf9e491b43eea587c0fce6ceb2df142cbfa559b
Dominique Normand <dnormand@us.imshealth.com>
2021-09-11 10:23:29 -0400

    143 done

---
+++ Solved/problem143.js
@@ -0,0 +1,62 @@
+const timeLogger = require('tools/timeLogger');
+const Tracer = require('tools/tracer');
+
+const MAX = 120000;
+
+function generatePairs()
+{
+    const pairs = [];
+    const indexes = [];
+
+    const add = (x, y) => {
+        let a = indexes[x];
+        if (! a) {
+            indexes[x] = a = new Set();
+        }
+        a.add(y);
+    }
+
+    const tracer = new Tracer(100, true);
+    for(let x = 1; x < MAX; x++) {
+        tracer.print(_ => MAX-x);
+        const x2 = x*x;
+        for(let y = x+1; x+y < MAX; y++) {
+            const v = Math.sqrt(x2 + y*y + x*y);
+            if (Math.floor(v) === v) {
+                add(x, y);
+                add(y, x);
+                pairs.push({ x, y });
+            }
+        }
+    }
+    tracer.clear();
+    return { pairs, indexes };
+}
+
+function solve()
+{
+    const { pairs, indexes } = timeLogger.wrap('Generating pairs', _ => generatePairs());    
+    const found = []
+    let total = 0;
+
+    for(let i = 0; i < pairs.length; i++) {
+        const { x, y } = pairs[i];      
+
+        const zs = [...indexes[x].values()].filter(z => indexes[y].has(z));
+
+        for(const z of zs) {
+            if (z !== x && z !== y && x+y+z <= MAX) {
+                const k = x+y+z;
+                if (! found[k]) {
+                    found[k] = 1;
+                    total += k;
+                }
+            }
+        }
+    }
+
+    return total;
+}
+
+const answer = timeLogger.wrap('', _ => solve());
+console.log(`Answer is ${answer}`);
