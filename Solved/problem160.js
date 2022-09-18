c45c5e2012ba0c2fada4ed1fa93684a2b3b77a64
Dominique Normand <dnormand@us.imshealth.com>
2022-09-18 09:48:53 -0400

    160 done

--- Solved/problem160.js
+++ Solved/problem160.js
@@ -1,5 +1,5 @@
 // Factorial trailing digits
-// Problem 160 
+// Problem 160
 // For any N, let f(N) be the last five digits before the trailing zeroes in N!.
 // For example,
 
@@ -11,46 +11,41 @@
 
 const assert = require('assert');
 const bigInt = require('big-integer');
-const prettyHrtime = require("pretty-hrtime");
+const prettyHrtime = require('pretty-hrtime');
 
-function f(max)
-{
-    const modulo = 1000000000;
-    const MOD = 100000;
+function f(max) {
+  const modulo = 1000000000;
+  const MOD = 100000;
 
-    let result = 1;
+  let result = 1;
 
-    while (max % MOD === 0 && max % 5 === 0)
-        max /= 5;
+  while (max % MOD === 0 && max % 5 === 0) max /= 5;
 
-    for (let value = 2; value <= max; value++)
-    {
-        let factor = value;
+  for (let value = 2; value <= max; value++) {
+    let factor = value;
 
-        // while (factor % 5 === 0 && result % 2 === 0)
-        // {
-        //     factor /= 5;
-        //     result /= 2;
-        // }
-        // while (factor % 2 === 0 && result % 5 === 0)
-        // {
-        //     factor /= 2;
-        //     result /= 5;
-        // }
+    // while (factor % 5 === 0 && result % 2 === 0)
+    // {
+    //     factor /= 5;
+    //     result /= 2;
+    // }
+    // while (factor % 2 === 0 && result % 5 === 0)
+    // {
+    //     factor /= 2;
+    //     result /= 5;
+    // }
 
-        while ((factor % 10) === 0)
-            factor /= 10;
+    while (factor % 10 === 0) factor /= 10;
 
-        result *= factor;
+    result *= factor;
 
-        while ((result % 10) === 0)
-            result /= 10;
+    while (result % 10 === 0) result /= 10;
 
-        result %= modulo;  
-    }
+    result %= modulo;
+  }
 
-    result %= MOD;
-    return result;
+  result %= MOD;
+  return result;
 }
 
 assert.equal(f(100), 16864);
@@ -63,4 +58,4 @@ assert.equal(f(10000000), 94688);
 let start = process.hrtime();
 let answer = f(1000000000000);
 let end = process.hrtime(start);
-console.log('f(1,000,000,000,000) = ' + answer + ", solved in " + prettyHrtime(end, {verbose:true}));
+console.log('f(1,000,000,000,000) = ' + answer + ', solved in ' + prettyHrtime(end, { verbose: true }));
