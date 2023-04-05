7c74a61b79de14ff83c14e7088dd2b30ccdee79c
Dominique Normand <dnormand@us.imshealth.com>
2023-04-05 06:54:02 -0400

    834 done

--- problem834.js
+++ problem834.js
@@ -1,148 +1,98 @@
 const assert = require('assert');
-const fs = require('fs');
-const { TimeLogger, Tracer } = require('@dn0rmand/project-euler-tools');
+const { TimeLogger, Tracer, primeHelper } = require('@dn0rmand/project-euler-tools');
 
 const MAX = 1234567;
-const MAXI = Math.floor(Math.sqrt(Number.MAX_SAFE_INTEGER)) + 1;
 
-class Total {
-  constructor(value) {
-    this.total = 0;
-    this.extra = BigInt(value || 0);
-  }
+primeHelper.initialize(MAX);
 
-  add(value) {
-    if (value instanceof Total) {
-      value = value.value;
-    }
-
-    if (typeof (value) === 'bigint') {
-      this.extra += value;
-    } else {
-      const t = this.total + value;
-      if (t > Number.MAX_SAFE_INTEGER) {
-        this.extra += BigInt(this.total);
-        this.total = value;
-      } else {
-        this.total = t;
-      }
-    }
-  }
-
-  get value() {
-    if (this.extra) {
-      const v = BigInt(this.total) + this.extra;
-      if (v <= Number.MAX_SAFE_INTEGER) {
-        return Number(v);
-      } else {
-        return v;
-      }
-    } else {
-      return this.total;
-    }
-  }
-}
-
-let n, n2, m1Div, m2Div;
-
-function init(N, start) {
-  n = N;
-  n2 = n + n;
-  [m1Div, m2Div] = start === 3 ? [2, 1] : [1, 2];
-}
-
-function sequence(m) {
+function S(m, n) {
   const modulo = m + n;
-  const m1 = (m + 1) / m1Div;
-  let m2 = (n2 + m) / m2Div;
-  if (m2 > modulo) {
-    m2 %= modulo;
-  }
-  if (m1 >= MAXI || m2 >= MAXI) {
-    const m1m2 = BigInt(m1) * BigInt(m2) % BigInt(modulo);
-    return Number(m1m2);
+  let t1 = (2 * n + m);
+  let t2 = (m + 1);
+  if (t2 & 1) {
+    t1 /= 2;
   } else {
-    return (m1 * m2) % modulo;
+    t2 /= 2;
+  }
+  const t = t1 * t2;
+  if (t > Number.MAX_SAFE_INTEGER) {
+    return Number((BigInt(t1) * BigInt(t2)) % BigInt(modulo));
+  } else {
+    return t % modulo;
   }
 }
 
-function processT(seq2, seq1, m) {
-  const k = seq2 - seq1 - 2;
+function getDivisors(n, callback) {
+  const primes = [];
+  primeHelper.factorize(n * (n - 1), (p, f) => {
+    primes.push({ prime: p, power: f });
+  });
 
-  if (k < -1) {
-    return m + 2;
-  }
-  if (k > 0) {
-    const jump = 2 * Math.floor((m + n - seq2) / k);
-    return m + (jump < 2 ? 2 : jump);
-  }
-  return -1;
-}
+  function inner(value, index) {
+    if (value > n) {
+      callback(value);
+    }
 
-function T(n, total) {
-  const max = n * n;
-
-  total = total || new Total();
-
-  for (const start of [3, 4]) {
-    init(n, start);
-
-    for (let m = start; m <= max;) {
-      const seq2 = sequence(m);
-
-      if (seq2 === 0) {
-        total.add(m);
-        m += 2;
-      } else {
-        const seq1 = sequence(m - 2);
-        m = processT(seq2, seq1, m);
-        if (m < 0) {
-          break;
-        }
+    for (let i = index; i < primes.length; i++) {
+      const { prime, power } = primes[i];
+      let v = value;
+      for (let p = 1; p <= power; p++) {
+        v *= prime;
+        inner(v, i + 1);
       }
     }
   }
+
+  inner(1, 0);
+}
+
+function T(n) {
+  let total = 0;
+
+  getDivisors(n, divisor => {
+    const m = divisor - n;
+    if (S(m, n) === 0) {
+      total += m;
+    }
+  });
 
   return total;
 }
 
 function U(N, trace) {
-  let total = new Total();
+  let total = 0;
+  let extra = 0n;
 
   const tracer = new Tracer(trace);
 
   let start = N;
 
-  // const fileName = './problem834.data';
-  // if (trace && fs.existsSync(fileName)) {
-  //   const data = JSON.parse(fs.readFileSync(fileName));
-  //   start = data.n;
-  //   total = new Total(data.total);
-  // }
-
   for (let n = start; n >= 3; n--) {
-    tracer.print(_ => {
-      // const data = JSON.stringify({
-      //   n, total: total.value.toString()
-      // }, null, 4);
-      // fs.writeFileSync(fileName, data);
-      return n;
-    });
-
-    T(n, total);
+    tracer.print(_ => n);
+    const t = T(n);
+    const subTotal = t + total;
+    if (subTotal < Number.MAX_SAFE_INTEGER) {
+      total = subTotal;
+    } else {
+      extra += BigInt(total) + BigInt(t);
+      total = 0;
+    }
   }
   tracer.clear();
 
-  return total.value;
+  if (extra) {
+    return extra + BigInt(total);
+  } else {
+    return total;
+  }
 }
 
-assert.strictEqual(T(10).value, 148);
-assert.strictEqual(T(100).value, 21828);
+assert.strictEqual(T(10), 148);
+assert.strictEqual(T(100), 21828);
 assert.strictEqual(U(100), 612572);
 assert.strictEqual(U(1000), 656827957);
 
 console.log('Tests passed');
-console.log(TimeLogger.wrap('', _ => U(12345, true)));
 
-// const answer = TimeLogger.wrap('', _ => U(MAX, true));
-// console.log(`Answer is ${answer} `);
+const answer = TimeLogger.wrap('', _ => U(MAX, true));
+console.log(`Answer is ${answer} `);
