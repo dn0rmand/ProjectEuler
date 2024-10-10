87c5e04a2ea71289d8a17bbb302397bb1fa2b928
dn0rmand <dn0rmand@users.noreply.github.com>
2024-10-10 11:40:53 -0400

    909 so much faster now

--- Solved/problem909.js
+++ Solved/problem909.js
@@ -1,15 +1,51 @@
 const assert = require("assert");
-const { TimeLogger } = require("@dn0rmand/project-euler-tools");
-const LExpression = require("../L-Expression");
 
-function solve(expression) {
-    return LExpression.evaluate(expression);
-}
+const MODULO = 1e9;
 
-assert.strictEqual(solve("S(Z)(A)(0)"), 1);
-assert.strictEqual(solve("S(S)(S(S))(S(Z))(A)(0)"), 6);
+let ids = 0;
+const $cache = new Map();
+
+const id = (f) => {
+    if (typeof f === "number") {
+        return "?";
+    } else if (f.id === undefined) {
+        f.id = ++ids;
+        return f.id;
+    } else {
+        return f.id;
+    }
+};
+
+const withId = (f) => {
+    id(f);
+    return f;
+};
+
+const S = withId((u) => (v) => (w) => {
+    if (typeof w === "number") {
+        const key = `${id(u)}:${id(v)}`;
+        let fn = $cache.get(key);
+        if (!fn) {
+            fn = v(u(v)(w));
+            if (typeof fn === "number") {
+                $cache.set(key, (fn + MODULO - w) % MODULO);
+            }
+            return fn;
+        } else {
+            return (w + fn) % MODULO;
+        }
+    } else {
+        return v(u(v)(w));
+    }
+});
+
+const Z = withId((u) => (v) => v);
+const A = withId((x) => (x + 1) % MODULO);
+
+assert.strictEqual(S(Z)(A)(0), 1);
+assert.strictEqual(S(S)(S(S))(S(Z))(A)(0), 6);
 
 console.log("Tests passed");
 
-const answer = TimeLogger.wrap("", () => solve("S(S)(S(S))(S(S))(S(Z))(A)(0)"));
+const answer = S(S)(S(S))(S(S))(S(Z))(A)(0);
 console.log(`Answer is ${answer}`);
